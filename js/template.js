dbd1a62ab96fb16572bf704e472d3e4bf7c81fa7
Johannes Stein <johannesstein@freeze-dev.de>
2012-07-01 22:53:22 +0200

    Added fresh scenes :)

--- js/template.js
+++ js/template.js
@@ -1,6 +1,9 @@
 ;(function(window, undefined) {
 	
 	var director = new Lyria.SceneDirector('viewport');
-	director.add('testScene');
+	director.add('scene1');
+	director.add('scene2');
+	
+	director.show('scene1');
 	
 })(window);
