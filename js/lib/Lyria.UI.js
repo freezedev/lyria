1c908dddff9a768116692d22ffef174294af9d68
Johannes Stein <johannesstein@freeze-dev.de>
2012-07-01 22:50:16 +0200

    Module structure for Lyria.UI

--- js/lib/Lyria.UI.js
+++ js/lib/Lyria.UI.js
@@ -2,58 +2,60 @@
  * @namespace Lyria
  * Lyria namespace decleration
  */
-var Lyria = Lyria || {};
+;(function(Lyria, undefined) {
 
 
-
-/**
- * @class Lyria.UI
- * User interface class
- */
-Lyria.UI = {};
-
-/**
- * 
- * @param {Object} element
- * @param {Object} options
- */
-Lyria.UI.Button = function(element, options) {
-	if(!element)
-		return;
-
-	var defaultOptions = {
-		caption: '',
-		buttonClass: 'button',
-		onClick: function(event) {
-		},
-		onMouseOver: function(event) {
-		},
-		onMouseOut: function(event) {
+	
+	/**
+	 * @class Lyria.UI
+	 * User interface class
+	 */
+	Lyria.UI = {};
+	
+	/**
+	 * 
+	 * @param {Object} element
+	 * @param {Object} options
+	 */
+	Lyria.UI.Button = function(element, options) {
+		if(!element)
+			return;
+	
+		var defaultOptions = {
+			caption: '',
+			buttonClass: 'button',
+			onClick: function(event) {
+			},
+			onMouseOver: function(event) {
+			},
+			onMouseOut: function(event) {
+			}
+		};
+		options = $.extend(true, defaultOptions, options);
+	
+		elementObj = (options.element instanceof jQuery) ? options.element : $(element);
+	
+		elementObj.addClass(options.buttonClass);
+		if(options.caption) {
+			
+			elementObj.html('<span>' + options.caption + '</span>');
 		}
-	};
-	options = $.extend(true, defaultOptions, options);
-
-	elementObj = (options.element instanceof jQuery) ? options.element : $(element);
-
-	elementObj.addClass(options.buttonClass);
-	if(options.caption) {
 		
-		elementObj.html('<span>' + options.caption + '</span>');
+		// Kill previously binded event listeners
+		//element.off();
+		
+		// Bind the events
+		//element.on
 	}
 	
-	// Kill previously binded event listeners
-	//element.off();
+	Lyria.UI.Overlay = {
 	
-	// Bind the events
-	//element.on
-}
-
-Lyria.UI.Lightbox = {
-
-}
-
-
-
-Lyria.UI.Dialog = {
+	}
 	
-}
+	
+	
+	Lyria.UI.Dialog = {
+		
+	}
+	
+})(window.Lyria = window.Lyria || {});
