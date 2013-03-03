0c79924c44757709ef5d275e74928b84db574100
Johannes Stein <johannesstein@freeze-dev.com>
2013-03-03 21:42:24 +0100

    Add Lyria.Viewport stub

---
+++ js/lyria/viewport.js
@@ -0,0 +1,17 @@
+/**
+ * @namespace Lyria
+ * Lyria namespace decleration
+ */
+;(function(Lyria, undefined) {
+  'use strict';
+
+  Lyria.Viewport = (function() {
+    
+    var Viewport = function() {};
+    
+    return Viewport;
+    
+  });
+
+
+})(this.Lyria = this.Lyria || {});
