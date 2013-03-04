52626957b0ff04b7d8827dbe935f9b7a54b0d1f6
Johannes Stein <johannesstein@freeze-dev.com>
2013-03-04 20:53:35 +0100

    Updated Lyria.Viewport

--- js/lyria/viewport.js
+++ js/lyria/viewport.js
@@ -7,11 +7,56 @@
 
   Lyria.Viewport = (function() {
     
-    var Viewport = function() {};
+    function Viewport(container, parent) {
+      
+      // Defaults container to the string 'viewport'
+      if (container == null) {
+        container = 'viewport';
+      }
+
+      if ($('#' + container).length > 0) {
+        this.$container = $('#' + container);
+      } else {
+        var createdElement = $(document.createElement('div')).attr('id', container);
+        
+        if (parent) {
+          $(parent).prepend(createdElement);
+        } else {
+          $('body').prepend(createdElement);
+        }
+        
+        this.$container = $('#' + container);
+      }
+      
+    }
+    
+    Viewport.prototype.scale = function(scaleX, scaleY) {
+      if (scaleX == null) {
+        return;
+      }
+      
+      if (scaleY == null) {
+        scaleY = scaleX;
+      }
+      
+      this.$container.css('transform', 'scale(' + scaleX + ',' + scaleY + ')');
+    };
+    
+    Viewport.prototype.center = function() {
+      
+    };
+    
+    Viewport.prototype.rotate = function(angle) {
+      if (angle == null) {
+        return;
+      }
+      
+      this.$container.css('transform', 'rotate(' + angle + ')');
+    };
     
     return Viewport;
     
-  });
+  })();
 
 
 })(this.Lyria = this.Lyria || {});
