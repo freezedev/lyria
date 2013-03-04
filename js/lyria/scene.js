19dc9d6237fc710bd195be7877786d33b52afdd0
Johannes Stein <johannesstein@freeze-dev.com>
2013-03-04 21:08:07 +0100

    Updated new scene function -> I should probably do that in a seperate branch

--- js/lyria/scene.js
+++ js/lyria/scene.js
@@ -7,35 +7,43 @@
 ;(function(window, Lyria, $, Handlebars, undefined) {
   'use strict';
 
-  Lyria.SceneNew = function(sceneName, sceneFunction, options) {
-    if (!sceneName) {
-      return;
-    }
+  Lyria.SceneNew = (function() {
     
-    var defaultOptions = {
-      target: '#' + sceneName,
-      template: 'scene.html',
-      path: 'scene',
-      partials: {},
-      helpers: {},
-      localization: 'localization.json',
-      parent: null,
-      route: '/' + sceneName,
-      isPrefab: false,
-      name: sceneName
+    var Scene = function(sceneName, sceneFunction, options) {
+      if (!sceneName) {
+        return;
+      }
+      
+      var defaultOptions = {
+        target: '#' + sceneName,
+        template: 'scene.html',
+        path: 'scene',
+        partials: {},
+        helpers: {},
+        localization: 'localization.json',
+        parent: null,
+        route: '/' + sceneName,
+        isPrefab: false,
+        name: sceneName
+      };
+      
+      options = $.extend(true, defaultOptions, options);
+      
+      var deferTemplate = $.Deferred(function(defer) {
+        
+      });
+      
+      var deferLoc = $.Deferred(function(defer) {
+        
+      });
+      
     };
     
-    options = $.extend(true, defaultOptions, options);
-    
-    var deferTemplate = $.Deferred(function(defer) {
+    Scene.prototype.add = function(gameObject) {
       
-    });
+    };
     
-    var deferLoc = $.Deferred(function(defer) {
-      
-    });
-    
-  };
+  })();
 
   var sceneCache = {};
 
