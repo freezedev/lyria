da266bc6a8a7fb6cfb7a3016ffb766f0c8a1aa5e
Johannes Stein <johannesstein@freeze-dev.com>
2013-03-06 23:23:06 +0100

    Prepare scenes script now serves a JavaScript file instead of a JSON

--- preparescenes.js
+++ preparescenes.js
@@ -5,7 +5,7 @@ var UglifyJS = require('uglify-js');
 
 var scenePath = path.join(__dirname, 'assets', 'scenes');
 
-var sceneObject = {};
+var sceneObject = '(function() {';
 var sceneList = fs.readdirSync(scenePath);
 
 sceneList.forEach(function(scene) {
@@ -15,30 +15,41 @@ sceneList.forEach(function(scene) {
       
       sceneObject[scene] = {};
       
+      sceneObject += 'Lyria.Scenes["' + scene + '"] = new Lyria.Scene("' + scene + '", function(sender) {';
+      
       var sceneFunc = path.join(scenePath, scene, 'scene.js');
       var sceneLoc = path.join(scenePath, scene, 'localization.json');
       var sceneMarkup = path.join(scenePath, scene, 'scene.html');
       
       if (fs.existsSync(sceneLoc)) {
         try {
-          sceneObject[scene]['localization'] = JSON.parse(fs.readFileSync(sceneLoc));          
+          sceneObject += 'var localization = sender.localization = ' + fs.readFileSync(sceneLoc) + ';';          
         } catch (e) {
           console.log('Error while evaluating ' + sceneLoc + ' :' + e);
         }
-      }
-      
-      if (fs.existsSync(sceneFunc)) {
-        sceneObject[scene]['content'] = UglifyJS.minify(fs.readFileSync(sceneFunc, 'utf8'), {fromString: true}).code;
+      } else {
+        sceneObject += 'var localization = sender.localization = {}';
       }
       
       if (fs.existsSync(sceneMarkup)) {
-        sceneObject[scene]['view'] = handlebars.precompile(fs.readFileSync(sceneMarkup, 'utf8'));
+        sceneObject += 'sender.template = ' + handlebars.precompile(fs.readFileSync(sceneMarkup, 'utf8')) + ';';
       }
+      
+      
+      if (fs.existsSync(sceneFunc)) {
+        sceneObject += fs.readFileSync(sceneFunc, 'utf8');
+      }
+      
+      
+      sceneObject += '})();'
     }
   }
   
 });
 
+sceneObject += '})();';
+
 console.log(sceneObject);
 
-fs.writeFileSync('scenes.js', JSON.stringify(sceneObject));
+fs.writeFileSync('scenes.js', sceneObject);
+fs.writeFileSync('scenes.min.js', UglifyJS.minify(sceneObject, {fromString: true}).code);
