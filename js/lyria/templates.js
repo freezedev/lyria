e6947fa826c043e4baf9d925001f16b756e42db8
Johannes Stein <johannesstein@freeze-dev.com>
2013-03-10 21:46:46 +0100

    TemplateConnector is now functional

--- js/lyria/templates.js
+++ js/lyria/templates.js
@@ -1,25 +1,55 @@
-(function(eos, Lyria) {
+(function(root, $, Lyria) {
+  
+  var noop = function() {};
+  var templateMethods = ['compile'];
+  
   
   Lyria.TemplateConnector = (function() {
     
-    var TemplateConnector = function(functionRefs) {
+    var TemplateConnector = function(functionRefs) {      
       if (typeof functionRefs === 'object') {
+        var key, value;
         
-        eos.each(functionRefs, function(key, value) {
+        for (key in functionRefs) {
+          value = functionRefs[key];
           
-        });
+          if (typeof value === 'function') {
+            this[key] = value;
+          }
+        }
         
       }
     };
+    
+    for (var i = 0, j = templateMethods.length; i < j; i++) {
+      (function(iterator) {
+        TemplateConnector.prototype[iterator] = noop;
+      })(templateMethods[i]);
+    }
+    
     
     return TemplateConnector;
     
   })();
   
-  Lyria.TemplateEngine = function(functionRefs) {
-    var noop = function() {};
-    
-    
+  Lyria.TemplateEngine = function(templateConnector) {
+    if (templateConnector instanceof Lyria.TemplateConnector) {
+      for (var i = 0, j = templateMethods.length; i < j; i++) {
+        (function(iterator) {
+          if ((templateConnector[iterator] != null) && (typeof templateConnector[iterator] === 'function')) {
+            Lyria.TemplateEngine[iterator] = templateConnector[iterator];
+          } else {
+            Lyria.TemplateEngine[iterator] = noop;
+          }
+        })(templateMethods[i]);
+      }
+    }
   };
   
-})(this.eos, this.Lyria = this.Lyria || {});
+  if (root.Handlebars) {
+    var handlebarsConnector = new Lyria.TemplateConnector({compile: Handlebars.compile});
+    
+    Lyria.TemplateEngine(handlebarsConnector);
+  }
+  
+})(this, this.jQuery, this.Lyria = this.Lyria || {});
