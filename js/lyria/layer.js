8272538958d6feb0bd7afe007050daf2c9123d10
Johannes Stein <johannesstein@freeze-dev.com>
2013-03-15 21:43:57 +0100

    Added layer stub

--- js/lyria/layer.js
+++ js/lyria/layer.js
@@ -4,5 +4,15 @@
  */
 ;(function(Lyria, undefined) {
 	'use strict';
+
+  Lyria.Layer = (function() {
+    
+    var Layer = function() {
+      
+    };
+    
+    return Layer;
+    
+  })();
 	
 })(window.Lyria = window.Lyria || {});
