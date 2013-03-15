e45114c7262a9ddfafbac04e1d06f4b2ae748438
Johannes Stein <johannesstein@freeze-dev.com>
2013-03-15 21:43:46 +0100

    Updated templates.js

--- js/lyria/templates.js
+++ js/lyria/templates.js
@@ -1,42 +1,41 @@
 (function(root, $, Lyria) {
-  
-  var noop = function() {};
+
+  var noop = function() {
+  };
   var templateMethods = ['compile'];
-  
-  
+
   Lyria.TemplateConnector = (function() {
-    
-    var TemplateConnector = function(functionRefs) {      
-      if (typeof functionRefs === 'object') {
+
+    var TemplateConnector = function(functionRefs) {
+      if ( typeof functionRefs === 'object') {
         var key, value;
-        
+
         for (key in functionRefs) {
           value = functionRefs[key];
-          
-          if (typeof value === 'function') {
+
+          if ( typeof value === 'function') {
             this[key] = value;
           }
         }
-        
+
       }
     };
-    
+
     for (var i = 0, j = templateMethods.length; i < j; i++) {
       (function(iterator) {
         TemplateConnector.prototype[iterator] = noop;
       })(templateMethods[i]);
     }
-    
-    
+
     return TemplateConnector;
-    
+
   })();
-  
+
   Lyria.TemplateEngine = function(templateConnector) {
-    if (templateConnector instanceof Lyria.TemplateConnector) {
+    if ( templateConnector instanceof Lyria.TemplateConnector) {
       for (var i = 0, j = templateMethods.length; i < j; i++) {
         (function(iterator) {
-          if ((templateConnector[iterator] != null) && (typeof templateConnector[iterator] === 'function')) {
+          if ((templateConnector[iterator] != null) && ( typeof templateConnector[iterator] === 'function')) {
             Lyria.TemplateEngine[iterator] = templateConnector[iterator];
           } else {
             Lyria.TemplateEngine[iterator] = noop;
@@ -45,11 +44,15 @@
       }
     }
   };
-  
+
   if (root.Handlebars) {
-    var handlebarsConnector = new Lyria.TemplateConnector({compile: Handlebars.compile});
-    
+    var handlebarsConnector = new Lyria.TemplateConnector({
+      compile: function() {
+        
+      }
+    });
+
     Lyria.TemplateEngine(handlebarsConnector);
   }
-  
+
 })(this, this.jQuery, this.Lyria = this.Lyria || {});
