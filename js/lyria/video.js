c26ca5bd481c520e18ec24ba85c9ca55b8824a78
Johannes Stein <johannesstein@freeze-dev.com>
2013-04-20 18:43:06 +0200

    Delete inline doc in lyria/video as it was messing with yuidoc

--- js/lyria/video.js
+++ js/lyria/video.js
@@ -1,14 +1,8 @@
-/**
- * @namespace Lyria
- * Lyria namespace decleration
- */
+
 define('lyria/video', function() {
   'use strict';
 
-  /**
-   * @class Lyria.Video
-   * This class creates an HTML5 video element
-   */
+
   return (function() {
     var Video = function() {
 
