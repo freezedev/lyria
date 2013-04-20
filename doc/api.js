98d6de34f777096e1a9a38097a26d67d7731d291
Johannes Stein <johannesstein@freeze-dev.com>
2013-04-20 18:07:29 +0200

    Added documentation

---
+++ doc/api.js
@@ -0,0 +1,10 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Lyria\nLyria namespace decleration.Lyria.Audio\nThis class creates an HTML5 audio element",
+        "Lyria\nLyria namespace decleration.Lyria.Video\nThis class creates an HTML5 video element"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
