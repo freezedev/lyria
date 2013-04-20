ea802ede61712d5620723f387291d3a6d0cbc966
Johannes Stein <johannesstein@freeze-dev.com>
2013-04-21 01:17:06 +0200

    https://gist.github.com/Stoney-FD/5427786

--- js/lyria/core.js
+++ js/lyria/core.js
@@ -1,30 +1,45 @@
-;(function(window, undefined) {
-  // frameRate is only used if requestAnimFrame is not available
-  window.frameRate = 60;
-  
-  /* shim layer with setTimeout fallback
-    http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-    http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
-  
-   http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
-  */
-  
+define('requestAnimationFrame', ['root'], function(root) {
+  // frameRate is only used if requestAnimationFrame is not available
+  var frameRate = 60;
+
+  var requestAnimationFrame = root.requestAnimationFrame;
+
   var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelRequestAnimationFrame = window[vendors[x]+
-          'CancelRequestAnimationFrame'];
-    }
+
+  for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+    requestAnimationFrame = root[vendors[x] + 'RequestAnimationFrame'];
     
-    if (!window.requestAnimFrame) {
-      window.requestAnimFrame = function( callback ){
-          window.setTimeout(callback, ~~(1000 / window.frameRate));
-        };
+    if (requestAnimationFrame) {
+      break;
     }
-    
-    if (!window.cancelAnimationFrame) {
-        window.cancelAnimationFrame = function(id) {
-            clearTimeout(id);
-        };
-    } 
-})(this);
+  }
+
+  if (!requestAnimationFrame) {
+    requestAnimationFrame = function(callback) {
+      window.setTimeout(callback, ~~(1000 / window.frameRate));
+    };
+  }
+
+  return requestAnimationFrame;
+});
+
+define('cancelAnimationFrame', ['root'], function(root) {
+
+  var cancelAnimationFrame = root.cancelAnimationFrame;
+
+  var vendors = ['ms', 'moz', 'webkit', 'o'];
+
+  for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+    cancelRequestAnimationFrame = root[vendors[x] + 'CancelRequestAnimationFrame'];
+  
+    if (cancelAnimationFrame) {
+      break;
+    }
+  }
+
+  if (!cancelAnimationFrame) {
+    cancelAnimationFrame = function(id) {
+      clearTimeout(id);
+    };
+  }
+});
