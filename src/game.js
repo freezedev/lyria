927813081e5e1830cf87906e762bcb54c6185a5f
Johannes Stein <johannesstein@freeze-dev.com>
2013-04-27 18:37:46 +0200

    lyria/game now has a functional preloader property

--- src/game.js
+++ src/game.js
@@ -2,7 +2,7 @@
  * @namespace Lyria
  * Lyria namespace decleration
  */
-define('lyria/game', ['lyria/viewport', 'lyria/scene/director'], function(Viewport, Director) {
+define('lyria/game', ['lyria/viewport', 'lyria/scene/director', 'lyria/preloader'], function(Viewport, Director, Preloader) {
   'use strict';
   
   // Lyria.Game
@@ -13,7 +13,7 @@ define('lyria/game', ['lyria/viewport', 'lyria/scene/director'], function(Viewpo
     
     Game.prototype.viewport = new Viewport();
     Game.prototype.director = new Director(Game.prototype.viewport);
-    Game.prototype.preloader = null;
+    Game.prototype.preloader = Preloader;
     
     return Game;
     
