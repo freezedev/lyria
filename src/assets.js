a1dd8e689b7a447157bbd56ca64e0f5c99944e2f
Johannes Stein <johannesstein@freeze-dev.com>
2013-04-28 13:18:48 +0200

    Updated reference to Resource in lyria/resource

--- src/assets.js
+++ src/assets.js
@@ -2,42 +2,48 @@
  * @namespace Lyria
  * Lyria namespace decleration
  */
-define('lyria/resource', {
-  /**
-   *
-   */
-  path: {
-    assets: "assets",
-    audio: "audio",
-    data: "data",
-    image: "images",
-    scene: "scenes",
-    video: "video",
-    prefab: "prefabs"
-  },
-
-  /**
-   *
-   *
-   */
-  name: function(filename, type) {
-    if (!filename) {
-      return;
+define('lyria/resource', function() {
+  
+  var Resource = {
+    /**
+     *
+     */
+    path: {
+      assets: "assets",
+      audio: "audio",
+      data: "data",
+      image: "images",
+      scene: "scenes",
+      video: "video",
+      prefab: "prefabs"
+    },
+  
+    /**
+     *
+     *
+     */
+    name: function(filename, type) {
+      if (!filename) {
+        return;
+      }
+  
+      var assetPath = Resource.path['assets'];
+      var typePath = "";
+  
+      if (Resource.path[type]) {
+        typePath = Resource.path[type];
+      } else {
+        typePath = type;
+      }
+  
+      if (typePath) {
+        return [assetPath, typePath.split('.').join('/'), filename].join('/');
+      } else {
+        return [assetPath, filename].join('/');
+      }
     }
-
-    var assetPath = Lyria.Resource.path['assets'];
-    var typePath = "";
-
-    if (Lyria.Resource.path[type]) {
-      typePath = Lyria.Resource.path[type];
-    } else {
-      typePath = type;
-    }
-
-    if (typePath) {
-      return [assetPath, typePath.split('.').join('/'), filename].join('/');
-    } else {
-      return [assetPath, filename].join('/');
-    }
-  }
+    
+  };
+  
+  return Resource;
 }); 
