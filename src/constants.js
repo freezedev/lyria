f17dd53804bca820435e806fbd16618d129f4648
Johannes Stein <johannesstein@freeze-dev.com>
2013-05-02 16:47:21 +0200

    Updated reference to detectr because it's an AMD module now

--- src/constants.js
+++ src/constants.js
@@ -8,7 +8,7 @@ define('lyria/constants', {
   animSpeed: 300
 });
 
-define('lyria/language', ['root'], function(root) {
+define('lyria/language', ['detectr'], function(detectr) {
   // Fallback language
   var defaultLanguage = 'en';
   
