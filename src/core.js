18e17ca1096bb3534aa000e470bb2235bb2e1c05
Johannes Stein <johannesstein@freeze-dev.com>
2013-05-02 22:32:15 +0200

    Added mixin definition

--- src/core.js
+++ src/core.js
@@ -4,6 +4,43 @@
   });
 })(this);
 
+define('mixin', function() {
+  var mixin, __slice = [].slice;
+
+  mixin = function() {
+    var key, oldRef, s, source, target, value, _i, _len;
+
+    target = arguments[0], source = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
+    if (!(target || source)) {
+      return;
+    }
+    for ( _i = 0, _len = source.length; _i < _len; _i++) {
+      s = source[_i];
+      for (key in s) {
+        value = s[key];
+        if (!Object.hasOwnProperty.call(target, key)) {
+          target[key] = value;
+        } /* else {
+          oldRef = target[key];
+          target[key] = (function() {
+            if ( typeof oldRef === 'function' && typeof value === 'function') {
+              return function() {
+                oldRef.apply(this, arguments);
+                return value.apply(this, arguments);
+              };
+            } else {
+              return [oldRef, value];
+            }
+          })();
+        } */
+      }
+    }
+    return null;
+  };
+
+  return mixin;
+});
+
 define('requestAnimationFrame', ['root'], function(root) {
   // frameRate is only used if requestAnimationFrame is not available
   var frameRate = 60;
@@ -14,7 +51,7 @@ define('requestAnimationFrame', ['root'], function(root) {
 
   for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
     requestAnimationFrame = root[vendors[x] + 'RequestAnimationFrame'];
-    
+
     if (requestAnimationFrame) {
       break;
     }
@@ -37,7 +74,7 @@ define('cancelAnimationFrame', ['root'], function(root) {
 
   for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
     cancelRequestAnimationFrame = root[vendors[x] + 'CancelRequestAnimationFrame'];
-  
+
     if (cancelAnimationFrame) {
       break;
     }
