059651cb316784d9cf9a1fd972a31cc036b35780
Johannes Stein <johannesstein@freeze-dev.com>
2013-05-13 23:04:17 +0200

    lyria/game updates its scene director

--- src/game.js
+++ src/game.js
@@ -3,7 +3,7 @@
  * 
  * @namespace Lyria
  */
-define('lyria/game', ['lyria/viewport', 'lyria/scene/director', 'lyria/preloader'], function(Viewport, Director, Preloader) {
+define('lyria/game', ['lyria/viewport', 'lyria/scene/director', 'lyria/preloader', 'lyria/loop'], function(Viewport, Director, Preloader, Loop) {
   'use strict';
   
   // Lyria.Game
@@ -11,11 +11,20 @@ define('lyria/game', ['lyria/viewport', 'lyria/scene/director', 'lyria/preloader
     
     // Constructor
     var Game = function() {
+      var self = this;
+      
       this.viewport = new Viewport();
       this.director = new Director(this.viewport);
       this.preloader = new Preloader();      
       this.preloader.sceneDirector = this.director;
+      this.loop = Loop;
+      
+      Game.Loop.addTask('update', function(dt) {
+        self.director.trigger('update', dt);
+      });      
     };
+    
+    Game.Loop = Loop;
     
     
     return Game;
