4d2443f8c8e5124f455740a4d47013a7c360ce43
Johannes Stein <johannesstein@freeze-dev.com>
2013-05-14 22:53:07 +0200

    Updated incorrect iterator reference + Better error handling for each module

--- src/core.js
+++ src/core.js
@@ -92,6 +92,10 @@ define('each', function() {
     var i, num, objKeys, val, _i, _j, _len, _len1;
 
     if (Array.isArray(obj)) {
+      if (obj.length === 0) {
+        return;
+      }
+      
       for ( num = _i = 0, _len = obj.length; _i < _len; num = ++_i) {
         i = obj[num];
         if (callback(num, i)) {
@@ -102,10 +106,14 @@ define('each', function() {
       }
     } else {
       objKeys = Object.keys(obj);
+      if (objKeys.length === 0) {
+        return;
+      }
+      
       for ( num = _j = 0, _len1 = objKeys.length; _j < _len1; num = ++_j) {
         i = objKeys[num];
-        val = obj[j];
-        if (callback(j, val)) {
+        val = obj[i];
+        if (callback(i, val)) {
           continue;
         } else {
           break;
