a00d35dcf094fe46d8392ad5c8a9fad98ac68906
Johannes Stein <johannesstein@freeze-dev.com>
2013-05-22 22:03:52 +0200

    Added clone function

---
+++ src/core/clone.js
@@ -0,0 +1,36 @@
+/*
+  Cloning objects
+*/
+define('clone', function() {
+  return function(obj) {
+    var flags, key, newInstance;
+
+    if ((obj == null) || typeof obj !== 'object') {
+      return obj;
+    }
+    if (obj instanceof Date) {
+      return new Date(obj.getTime());
+    }
+    if (obj instanceof RegExp) {
+      flags = '';
+      if (obj.global != null) {
+        flags += 'g';
+      }
+      if (obj.ignoreCase != null) {
+        flags += 'i';
+      }
+      if (obj.multiline != null) {
+        flags += 'm';
+      }
+      if (obj.sticky != null) {
+        flags += 'y';
+      }
+      return new RegExp(obj.source, flags);
+    }
+    newInstance = new obj.constructor();
+    for (key in obj) {
+      newInstance[key] = clone(obj[key]);
+    }
+    return newInstance;
+  };
+});
