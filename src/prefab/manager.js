0afbe9fc716f74eca4ee734e9644d938fb0ef694
Johannes Stein <johannesstein@freeze-dev.com>
2013-05-24 21:50:07 +0200

    Added prefab manager stub

--- src/prefab/manager.js
+++ src/prefab/manager.js
@@ -1,5 +1,11 @@
 define('lyria/prefab/manager', function() {
   var PrefabManager = {};
   
+  PrefabManager.create = function(name) {
+    if (PrefabManager.precompiledPrefabs) {
+      return PrefabManager.precompiledPrefabs[name];
+    }
+  };
+  
   return PrefabManager;
 });
