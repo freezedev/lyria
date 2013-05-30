404ac8e1c1713c682a73cd51245a746874eee3ff
Johannes Stein <johannesstein@freeze-dev.com>
2013-05-30 16:53:19 +0200

    Added basic data store functionality

--- src/data/store.js
+++ src/data/store.js
@@ -1,3 +1,20 @@
-define('lyria/data/store', function() {
-  
+define('lyria/data/store', ['lyria/eventmap'], function(EventMap) {
+
+  var data = {};
+  var eventMap = new EventMap();
+
+  var DataStore = {
+    has: function(key) {
+      return data.hasOwnProperty('key');
+    },
+    get: function(key) {
+      return data[key];
+    },
+    set: function(key, value) {
+      data[key] = value;
+    }
+  };
+
+  return DataStore;
+
 });
