f35121e3d7c77ba2708f939713780cb50783662a
Johannes Stein <johannesstein@freeze-dev.com>
2013-05-30 16:53:47 +0200

    Added data store dependency

--- src/scene/index.js
+++ src/scene/index.js
@@ -52,14 +52,15 @@ define('lyria/scene', ['isEmptyObject', 'each', 'extend', 'clone', 'mixin', 'lyr
       };
       
       // Call scene
-      require(['lyria/achievements', 'lyria/log', 'lyria/component', 'lyria/gameobject', 'lyria/events', 'lyria/resource'], function(Achievements, Log, Component, GameObject, Events, Resource) {
+      require(['lyria/achievements', 'lyria/log', 'lyria/component', 'lyria/gameobject', 'lyria/events', 'lyria/resource', 'lyria/data/store'], function(Achievements, Log, Component, GameObject, Events, Resource, DataStore) {
         var LyriaObject = {
           Achievements: Achievements,
           Log: Log,
           Component: Component,
           GameObject: GameObject,
           Events: Events,
-          Resource: Resource
+          Resource: Resource,
+          DataStore: DataStore
         };
         
         sceneFunction.apply(self, [self, LyriaObject]);
