a6ff3af5685939e5467f2a73cbacfc2ff5b72624
Johannes Stein <johannesstein@freeze-dev.com>
2013-06-12 20:35:07 +0200

    Added JSDoc-like comments

--- src/resource.js
+++ src/resource.js
@@ -19,8 +19,10 @@ define('lyria/resource', function() {
     },
   
     /**
-     *
-     *
+     * Returns the relative filename to an asset by filename and type
+     * 
+     * @param {String} filename
+     * @param {String} type
      */
     name: function(filename, type) {
       if (!filename) {
