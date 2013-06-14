d24abb5313391cfb7823f999e2d37686bdf468aa
Johannes Stein <johannesstein@freeze-dev.com>
2013-06-14 22:36:14 +0200

    Added deleteItem module

---
+++ src/core/deleteitem.js
@@ -0,0 +1,25 @@
+define('deleteitem', function() {
+  var deleteItem = function(obj, item) {
+    var i, key, newObject, num, _i, _len, _results;
+    if (Array.isArray(obj)) {
+      _results = [];
+      for ( num = _i = 0, _len = obj.length; _i < _len; num = ++_i) {
+        i = obj[num];
+        if (num !== item) {
+          _results.push(i);
+        }
+      }
+      return _results;
+    } else {
+      newObject = {};
+      for (key in obj) {
+        if (key !== item) {
+          newObject[key] = obj[key];
+        }
+      }
+      return newObject;
+    }
+  };
+
+  return deleteItem;
+});
