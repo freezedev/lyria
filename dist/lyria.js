c2bfa91607b217f44da58757184ad35ec87ad725
Johannes Stein <johannesstein@freeze-dev.com>
2013-06-23 20:19:32 +0200

    Updated dist JS

--- dist/lyria.js
+++ dist/lyria.js
@@ -1096,7 +1096,7 @@ define('lyria/layer', ['mixin', 'lyria/gameobject'], function(mixin, GameObject)
   })();
 }); 
 define('lyria/localization/global', ['lyria/localization', 'lyria/resource'], function(Localization, Resource) {
-  var instance = instance || new Localization(Resource.name("i18n.json"));
+  var instance = instance || new Localization(Resource.name('i18n.json'));
   
   return instance;
 });
@@ -1761,8 +1761,6 @@ define('lyria/scene/director', ['root', 'mixin', 'jquery', 'lyria/eventmap', 'ly
  */
 define('lyria/scene', ['isEmptyObject', 'each', 'extend', 'clone', 'mixin', 'lyria/eventmap', 'lyria/gameobject'], function(isEmptyObject, each, extend, clone, mixin, EventMap, GameObject) {'use strict';
 
-  var sceneCache = {};
-
   var Scene = (function() {
 
     /**
@@ -1940,6 +1938,56 @@ define('lyria/scene', ['isEmptyObject', 'each', 'extend', 'clone', 'mixin', 'lyr
   return Scene;
 
 }); 
+define('lyria/serialize', ['jquery'], function($) {
+  /**
+   *
+   * @param {Object} anyObject
+   *
+   * @returns {String}
+   */
+  var serialize = function(anyObject) {
+    if (( typeof anyObject !== 'object') || ( anyObject instanceof $)) {
+      return;
+    }
+
+    var str = '{';
+
+    for (var p in anyObject) {
+      if (anyObject.hasOwnProperty(p)) {
+        if (anyObject[p] instanceof $) {
+          continue;
+        }
+
+        var objKeys = Object.keys(anyObject);
+        var commaStr = (objKeys.indexOf(p) === (objKeys.length - 1)) ? '' : ',';
+
+        switch (typeof anyObject[p]) {
+          case 'object':
+            {
+              str += p + ': ' + serialize(anyObject[p]) + commaStr + '\n';
+            }
+            break;
+          case 'string':
+            {
+              str += p + ': "' + anyObject[p] + '"' + commaStr + '\n';
+            }
+            break;
+          default:
+            {
+              str += p + ': ' + anyObject[p] + commaStr + '\n';
+            }
+            break;
+        }
+      }
+    }
+    str += '}';
+
+    return str;
+  };
+
+  return serialize;
+});
+
 /**
  * @module Lyria
  * @submodule Template 
@@ -2060,58 +2108,6 @@ define('lyria/utils', ['jquery'], function($) {
     } else {
       return false;
     }
-  };
-  /**
-   *
-   * @param {Object} anyObject
-   *
-   * @returns {Object}
-   */
-  Utils.cloneObject = function(anyObject) {
-    return $.extend(true, {}, anyObject);
-  };
-  
-  /**
-   * 
-   * @param {Object} anyObject
-   * 
-   * @returns {String}
-   */
-  Utils.serializeObject = function(anyObject) {
-    if ((typeof anyObject !== 'object') || (anyObject instanceof $)) {
-      return;
-    }
-    
-      var str = '{';        
-      
-      for (var p in anyObject) {
-          if (anyObject.hasOwnProperty(p)) {
-            if (anyObject[p] instanceof $) {
-              continue;
-            }
-            
-            var objKeys = Object.keys(anyObject);
-            var commaStr = (objKeys.indexOf(p) === (objKeys.length - 1)) ? '' : ',';
-            
-            switch (typeof anyObject[p]) {
-              case 'object': {
-                str += p + ': ' + Utils.serializeObject(anyObject[p]) + commaStr + '\n';
-              }
-                break;
-              case 'string': {
-                str += p + ': "' + anyObject[p] + '"' + commaStr + '\n';
-              }
-                break;
-              default: {
-                str += p + ': ' + anyObject[p] + commaStr + '\n';
-              }
-                break;
-            }
-          }
-      }
-      str += '}';
-      
-      return str;
   };
   
   return Utils;
