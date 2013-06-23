4daa4224588bd32801a4fddff2ee4232e3bab5d7
Johannes Stein <johannesstein@freeze-dev.com>
2013-06-23 20:18:55 +0200

    Moved serialize function into seperate module

---
+++ src/serialize.js
@@ -0,0 +1,49 @@
+define('lyria/serialize', ['jquery'], function($) {
+  /**
+   *
+   * @param {Object} anyObject
+   *
+   * @returns {String}
+   */
+  var serialize = function(anyObject) {
+    if (( typeof anyObject !== 'object') || ( anyObject instanceof $)) {
+      return;
+    }
+
+    var str = '{';
+
+    for (var p in anyObject) {
+      if (anyObject.hasOwnProperty(p)) {
+        if (anyObject[p] instanceof $) {
+          continue;
+        }
+
+        var objKeys = Object.keys(anyObject);
+        var commaStr = (objKeys.indexOf(p) === (objKeys.length - 1)) ? '' : ',';
+
+        switch (typeof anyObject[p]) {
+          case 'object':
+            {
+              str += p + ': ' + serialize(anyObject[p]) + commaStr + '\n';
+            }
+            break;
+          case 'string':
+            {
+              str += p + ': "' + anyObject[p] + '"' + commaStr + '\n';
+            }
+            break;
+          default:
+            {
+              str += p + ': ' + anyObject[p] + commaStr + '\n';
+            }
+            break;
+        }
+      }
+    }
+    str += '}';
+
+    return str;
+  };
+
+  return serialize;
+});
--- src/utils.js
+++ src/utils.js
@@ -30,49 +30,6 @@ define('lyria/utils', ['jquery'], function($) {
     }
   };
   
-  /**
-   * 
-   * @param {Object} anyObject
-   * 
-   * @returns {String}
-   */
-  Utils.serializeObject = function(anyObject) {
-    if ((typeof anyObject !== 'object') || (anyObject instanceof $)) {
-      return;
-    }
-    
-      var str = '{';        
-      
-      for (var p in anyObject) {
-          if (anyObject.hasOwnProperty(p)) {
-            if (anyObject[p] instanceof $) {
-              continue;
-            }
-            
-            var objKeys = Object.keys(anyObject);
-            var commaStr = (objKeys.indexOf(p) === (objKeys.length - 1)) ? '' : ',';
-            
-            switch (typeof anyObject[p]) {
-              case 'object': {
-                str += p + ': ' + Utils.serializeObject(anyObject[p]) + commaStr + '\n';
-              }
-                break;
-              case 'string': {
-                str += p + ': "' + anyObject[p] + '"' + commaStr + '\n';
-              }
-                break;
-              default: {
-                str += p + ': ' + anyObject[p] + commaStr + '\n';
-              }
-                break;
-            }
-          }
-      }
-      str += '}';
-      
-      return str;
-  };
-  
   return Utils;
   
 });
