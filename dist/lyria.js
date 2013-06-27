b3f03c4856c31792b031ec899d1029c8e1a08afb
Johannes Stein <johannesstein@freeze-dev.com>
2013-06-27 22:46:44 +0200

    Updated dist JS

--- dist/lyria.js
+++ dist/lyria.js
@@ -742,6 +742,60 @@ define('mixin', function() {
 
   return mixin;
 });
+define('path', function() {
+  var delimiter = '/';
+
+  var Path = {
+    join: function(arr) {
+      var pathArray = (function() {
+        var result = [];
+
+        for (var i = 0, j = arr.length; i < j; i++) {
+          if (arr[i] != null && arr[i] !== '') {
+            result.push(arr[i]);
+          }
+        }
+
+        return result;
+      })();
+      return pathArray.join(delimiter);
+    },
+    resolve: function(base, relativeStr) {
+      if (!Array.isArray(relativeStr)) {
+        relativeStr = relativeStr.split(delimiter);
+      }
+
+      var baseArray = base.split(delimiter);
+      var result = [];
+
+      var completeArr = baseArray.concat(relativeStr);
+
+      for (var i = 0, j = completeArr.length; i < j; i++) {
+        if (completeArr[i] != null && completeArr[i] !== '') {
+          if (completeArr[i] === '.') {
+            continue;
+          }
+
+          if (completeArr[i + 1] === '..') {
+            if (i + 1 !== j) {
+              completeArr[i] = '';       
+            }
+            completeArr[i + 1] = '';
+
+            continue;
+          }
+
+          result.push(completeArr[i]);
+        }
+      }
+
+      return Path.join(result);
+    }
+  };
+
+  return Path;
+});
+
 (function(root) {
   define('root', function() {
     return root;
@@ -1939,6 +1993,8 @@ define('lyria/scene', ['isEmptyObject', 'each', 'extend', 'clone', 'mixin', 'lyr
 
 }); 
 define('lyria/serialize', ['jquery'], function($) {
+  
+  // TODO: Use JSON.parse + reviver instead
   /**
    *
    * @param {Object} anyObject
