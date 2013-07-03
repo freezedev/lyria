e23e9a82e28b3cb31a9da842c3ebb2f68d62651e
Johannes Stein <johannesstein@freeze-dev.com>
2013-07-03 23:22:46 +0200

    Added more straightforward random function

---
+++ src/core/random.js
@@ -0,0 +1,13 @@
+define('random', function() {
+  return function(max, min) {
+    if (max == null) {
+      max = 1.0;
+    }
+    
+    if (min == null) {
+      min = 0.0;
+    }
+    
+    return Math.floor(Math.random() * max) + min;
+  };
+});
