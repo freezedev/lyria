e8ed8965e15feec2cceba1485e0772dc82463373
Johannes Stein <johannesstein@freeze-dev.com>
2013-07-04 23:22:24 +0200

    Added clamp module

---
+++ src/core/clamp.js
@@ -0,0 +1,31 @@
+define('clamp', function() {
+  var clamp = function(value, min, max) {
+    var _ref, _ref1, _ref2;
+    if ( typeof value === 'object') {
+      _ref = value, min = _ref.min, max = _ref.max, value = _ref.value;
+    }
+    if (Array.isArray(min)) {
+      _ref1 = min, min = _ref1[0], max = _ref1[1];
+    }
+    if (min == null) {
+      min = 0.0;
+    }
+    if (max == null) {
+      max = 1.0;
+    }
+    if (min > max) {
+      _ref2 = [max, min], min = _ref2[0], max = _ref2[1];
+    }
+    if ((min <= value && value <= max)) {
+      return value;
+    } else {
+      if (value > max) {
+        return max;
+      } else {
+        return min;
+      }
+    }
+  };
+
+  return clamp;
+});
