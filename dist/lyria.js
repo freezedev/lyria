ee10f2676b20baede61cdaf8600bffded3b0f604
Johannes Stein <johannesstein@freeze-dev.com>
2013-07-04 23:26:20 +0200

    Updated dist JS

--- dist/lyria.js
+++ dist/lyria.js
@@ -552,6 +552,38 @@ define('lyria/language', ['detectr', 'lyria/events'], function(detectr, Events) 
     return cancelAnimationFrame;
   });
 })();
+define('clamp', function() {
+  var clamp = function(value, min, max) {
+    var _ref, _ref1, _ref2;
+    if ( typeof value === 'object') {
+      _ref = value, min = _ref.min, max = _ref.max, value = _ref.value;
+    }
+    if (Array.isArray(min)) {
+      _ref1 = min, min = _ref1[0], max = _ref1[1];
+    }
+    if (min == null) {
+      min = 0.0;
+    }
+    if (max == null) {
+      max = 1.0;
+    }
+    if (min > max) {
+      _ref2 = [max, min], min = _ref2[0], max = _ref2[1];
+    }
+    if ((min <= value && value <= max)) {
+      return value;
+    } else {
+      if (value > max) {
+        return max;
+      } else {
+        return min;
+      }
+    }
+  };
+
+  return clamp;
+});
+
 /*
   Cloning objects
 */
