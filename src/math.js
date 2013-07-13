539ad09fa2ed02a7694e05302a13053a8a530ca9
Johannes Stein <johannesstein@freeze-dev.com>
2013-07-14 00:31:14 +0200

    lyria/math just references random, clamp and fisherYates modules

--- src/math.js
+++ src/math.js
@@ -1,42 +1,9 @@
-define('lyria/math', ['root'], function(root) {
+define('lyria/math', ['root', 'random', 'clamp', 'fisheryates'], function(root, random, clamp, fisheryates) {
 
   var Math = {
-    random: function(max, min) {
-      if (max == null) {
-        max = 1.0;
-      }
-      
-      if (min == null) {
-        min = 0.0;
-      }
-      
-      return root.Math.random() * max + min;
-    },
-    clamp: function(value, min, max) {
-      var _ref, _ref1;
-
-      if ( typeof value === 'object') {
-        _ref = value, min = _ref.min, max = _ref.max, value = _ref.value;
-      }
-      if (min == null) {
-        min = 0.0;
-      }
-      if (max == null) {
-        max = 1.0;
-      }
-      if (min > max) {
-        _ref1 = [max, min], min = _ref1[0], max = _ref1[1];
-      }
-      if ((min <= value && value <= max)) {
-        return value;
-      } else {
-        if (value > max) {
-          return max;
-        } else {
-          return min;
-        }
-      }
-    }
+    random: random,
+    clamp: clamp,
+    fisherYates: fisheryates
   };
 
   return Math;
