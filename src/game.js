65f59624cfa17fec410287d45575f116590ad5e0
Johannes Stein <johannesstein@freeze-dev.com>
2013-07-19 20:28:56 +0200

    Updated inline documentation in lyria/game

--- src/game.js
+++ src/game.js
@@ -1,24 +1,41 @@
 /**
- * Lyria namespace decleration
- * 
- * @namespace Lyria
+ * @module Lyria
  */
 define('lyria/game', ['lyria/viewport', 'lyria/scene/director', 'lyria/preloader', 'lyria/loop'], function(Viewport, Director, Preloader, Loop) {
   'use strict';
   
-  // Lyria.Game
+  /**
+   * Game class which has a viewport, scene director and preloader by
+   * default.
+   *
+   * @class Game 
+   */
   return (function() {
     
-    // Constructor
+    /**
+     * @constructor 
+     */
     var Game = function() {
       var self = this;
       
+      /**
+       * @property viewport
+       * @type {Viewport} 
+       */
       // Set up a viewport
       this.viewport = new Viewport();
-      
+
+      /**
+       * @property director
+       * @type {Director} 
+       */      
       // Add a scene director
       this.director = new Director(this.viewport);
       
+      /**
+       * @property preloader
+       * @type {Preloader} 
+       */
       // Add a preloader
       this.preloader = new Preloader(); 
       
@@ -31,6 +48,11 @@ define('lyria/game', ['lyria/viewport', 'lyria/scene/director', 'lyria/preloader
       });      
     };
     
+    /**
+     * @property Loop
+     * @static
+     * @type {Loop} 
+     */
     // Store the reference to the Lyria Loop at the Game object
     Game.Loop = Loop;
     
