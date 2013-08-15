684dba17ad66efc76b6ea154dfa35723d0cee457
Johannes Stein <johannesstein@freeze-dev.com>
2013-08-15 18:06:00 +0200

    Added string templating (Parametrized strings)

---
+++ src/template/string.js
@@ -0,0 +1,34 @@
+define('lyria/template/string', function() {
+  var templateString = {
+    key: {
+      start: '{{',
+      end: '}}'
+    },
+    process: function(value, parameter) {
+      if (value == null) {
+        return;
+      }
+
+      // Array or object
+      if ( typeof parameter === 'object') {
+        if (Array.isArray(parameter)) {
+          for (var i = 0, j = parameter.length; i < j; i++) {
+            value = value.replace(new RegExp(templateString.key.start + i + templateString.key.end), parameter[i]);
+          }
+        } else {
+          var paramKeys = Object.keys(parameter);
+
+          for (var k = 0, l = paramKeys.length; k < l; k++) {
+            (function(item) {
+              value = value.replace(new RegExp(templateString.key.start + paramKeys[k] + templateString.key.end), item);
+            })(parameter[paramKeys[k]]);
+          }
+        }
+      }
+
+      return value;
+    }
+  };
+
+  return templateString;
+});
