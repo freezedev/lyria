2bac5400266b79671551a74d2d1d8083a02d0c81
Johannes Stein <johannesstein@freeze-dev.com>
2013-08-16 18:47:26 +0200

    Updated dist JS

--- dist/js/lyria.js
+++ dist/js/lyria.js
@@ -475,7 +475,7 @@ define('lyria/constants', function() {
   };
 });
 
-define('lyria/language', ['detectr', 'lyria/events'], function(detectr, Events) {
+define('lyria/language', ['detectr', 'lyria/events', 'lyria/mixin/language'], function(detectr, Events, langMixin) {
   // Fallback language
   var langObject = {
     defaultLanguage: 'en'
@@ -483,17 +483,7 @@ define('lyria/language', ['detectr', 'lyria/events'], function(detectr, Events) 
 
   var langProp = detectr.Browser.language() || langObject.defaultLanguage;
 
-  Object.defineProperty(langObject, 'language', {
-    get: function() {
-      return langProp;
-    },
-    set: function(value) {
-      langProp = value;
-      Events.trigger('language:change', langProp);
-    },
-    configurable: true,
-    enumarable: true
-  });
+  langMixin(langObject, langProp, Events);
 
   return langObject;
 });
@@ -980,172 +970,7 @@ define('lyria/data/store', ['lyria/eventmap'], function(EventMap) {
  * @namespace Lyria
  * Lyria namespace decleration
  */
-define('lyria/eventmap', ['root'], function(root) {'use strict';
-
-  /**
-   * This is directly taken from
-   * https://github.com/elysion-powered/elyssa/blob/master/src/core/events.coffee
-   */
-  var EventMap = (function() {
-    function EventMap(sender) {
-      this.sender = sender;
-      this.events = {};
-      this.validEvents = [];
-    }
-
-
-    EventMap.prototype.serialize = function() {
-      var err, result;
-      try {
-        result = JSON.stringify(this.events, function(key, value) {
-          if ( typeof value === 'function') {
-            value = value.toString();
-          }
-          return value;
-        });
-      } catch (_error) {
-        err = _error;
-        console.error("Error while serializing eventmap: " + err);
-      }
-      return result;
-    };
-
-    EventMap.prototype.deserialize = function(string) {
-      /*jshint evil:true*/
-      var err, events;
-      try {
-        events = JSON.parse(string, function(key, value) {
-          if (value.indexOf('function') === 0) {
-            value = new Function(value)();
-          }
-          return value;
-        });
-      } catch (_error) {
-        err = _error;
-        console.error("Error while deserializing eventmap: " + err);
-        return false;
-      }
-      this.events = events;
-      return true;
-    };
-
-    EventMap.prototype.on = function(eventName, eventFunction) {
-      var eventDesc;
-      if (!eventFunction) {
-        return;
-      }
-      if (this.validEvents.length > 0) {
-        if (this.validEvents.indexOf(eventName) === -1) {
-          return;
-        }
-      }
-      eventDesc = {
-        event: eventFunction,
-        id: -1,
-        type: '',
-        sender: this.sender
-      };
-      if (!this.events[eventName]) {
-        this.events[eventName] = [eventDesc];
-      } else {
-        this.events[eventName].push(eventDesc);
-      }
-      return this;
-    };
-
-    EventMap.prototype.off = function(eventName) {
-      var eventType;
-      if (!eventName) {
-        return;
-      }
-      eventType = this.events[eventName].type;
-      if (eventType === 'once' || eventType === 'repeat') {
-        if (eventType === 'repeat') {
-          root.clearInterval(this.events[eventName].id);
-        }
-        if (eventType === 'once') {
-          root.clearTimeout(this.events[eventName].id);
-        }
-      }
-      if (this.events[eventName]) {
-        delete this.events[eventName];
-      }
-      return this;
-    };
-
-    EventMap.prototype.clear = function() {
-      this.events = {};
-      this.validEvents = [];
-      return this;
-    };
-
-    EventMap.prototype.trigger = function() {
-      var args, context, delay, eventName, i, interval, name, repeat, timeoutId, triggerEvent, triggerFunction, _i, _len, _ref;
-      eventName = arguments[0], args = 2 <= arguments.length ? [].slice.call(arguments, 1) : [];
-      if (eventName == null) {
-        return;
-      }
-      if ( typeof eventName === 'object') {
-        name = eventName.name, interval = eventName.interval, repeat = eventName.repeat, context = eventName.context, delay = eventName.delay;
-      } else {
-        name = eventName;
-      }
-      if (!this.events[name]) {
-        return;
-      }
-      if (interval == null) {
-        interval = 0;
-      }
-      if (repeat == null) {
-        repeat = false;
-      }
-      if (context == null) {
-        context = this;
-      }
-      if (delay == null) {
-        delay = 0;
-      }
-      _ref = this.events[name];
-      for ( _i = 0, _len = _ref.length; _i < _len; _i++) {
-        i = _ref[_i];
-        triggerFunction = function() {
-          if (i.sender) {
-            return i.event.apply(context, [].concat.apply([], [[i.sender], args]));
-          } else {
-            return i.event.apply(context, args);
-          }
-        };
-        triggerEvent = function() {
-          if (interval) {
-            if (repeat) {
-              i.type = 'repeat';
-              i.id = root.setInterval(triggerFunction, interval);
-            } else {
-              i.type = 'once';
-              i.id = root.setTimeout(triggerFunction, interval);
-            }
-          } else {
-            i.type = 'direct';
-            triggerFunction.call(this);
-          }
-          return null;
-        };
-        if (delay) {
-          timeoutId = root.setTimeout(function() {
-            triggerEvent.call(this);
-            return root.clearTimeout(timeoutId);
-          });
-        } else {
-          triggerEvent.call(this);
-        }
-      }
-      return this;
-    };
-
-    return EventMap;
-
-  })();
-
+define('lyria/eventmap', ['eventmap'], function(EventMap) {
   return EventMap;
 });
 
@@ -1624,6 +1449,27 @@ define('lyria/math', ['root', 'random', 'clamp', 'fisheryates'], function(root, 
 });
 
 /**
+ * Mixin language property into objects 
+ */
+
+define('lyria/mixin/language', function() {
+  return function(attachedObject, value, eventMap) {
+
+    Object.defineProperty(attachedObject, 'language', {
+      get: function() {
+        return value;
+      },
+      set: function(val) {
+        value = val;
+        eventMap.trigger('language:change', value);
+      },
+      configurable: true,
+      enumarable: true
+    });
+
+  };
+}); 
+/**
  * @namespace Lyria
  * Lyria namespace decleration
  */
@@ -2034,7 +1880,7 @@ define('lyria/scene/director', ['root', 'mixin', 'jquery', 'lyria/eventmap', 'ly
 /**
  * @module Lyria
  */
-define('lyria/scene', ['jquery', 'isemptyobject', 'each', 'extend', 'clone', 'mixin', 'nexttick', 'lyria/eventmap', 'lyria/gameobject', 'lyria/language', 'lyria/template/string', 'lyria/log'], function($, isEmptyObject, each, extend, clone, mixin, nextTick, EventMap, GameObject, Language, templateString, Log) {'use strict';
+define('lyria/scene', ['jquery', 'isemptyobject', 'each', 'extend', 'clone', 'mixin', 'nexttick', 'lyria/eventmap', 'lyria/gameobject', 'lyria/language', 'lyria/template/string', 'lyria/log', 'lyria/mixin/language'], function($, isEmptyObject, each, extend, clone, mixin, nextTick, EventMap, GameObject, Language, templateString, Log, langMixin) {'use strict';
 
   var Scene = (function() {
 
@@ -2068,7 +1914,10 @@ define('lyria/scene', ['jquery', 'isemptyobject', 'each', 'extend', 'clone', 'mi
 
       // Default values
       this.localization = {};
-      this.language = Language.language;
+      
+      var langValue = Language.language;
+      
+      langMixin(self, langValue, self);
 
       this.template = {};
       this.template.source = '';
@@ -2314,15 +2163,33 @@ define('lyria/scene', ['jquery', 'isemptyobject', 'each', 'extend', 'clone', 'mi
      *
      * @param {Object} lang
      */
-    Scene.prototype.i18n = function(name, parameter) {
+    Scene.prototype.t = function(name, parameter) {
+      var self = this;
+
       if (this.localization && this.localization[this.language]) {
-        return templateString.process(this.localization[this.language][name], parameter);
+        if (this.localization[this.language][name] == null) {
+          return '[[Missing localization for ' + name + ']]';
+        }
+
+        if ( typeof this.localization[this.language][name] === 'string') {
+          return templateString.process(this.localization[this.language][name], parameter);
+        } else {
+          return {
+            plural: function(n) {
+              if (self.localization[this.language][name][n]) {
+                return templateString.process(self.localization[self.language][name][n], parameter);
+              } else {
+                return templateString.process(self.localization[self.language][name]['n'], parameter);
+              }
+            }
+          };
+        }
       }
     };
-    
+
     /**
      * Logging directly from the scene
-     *  
+     *
      * @param {Object} text
      */
     Scene.prototype.log = function(text) {
