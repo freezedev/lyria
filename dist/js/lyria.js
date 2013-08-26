6c58a27ed663c4b86a1e976459007c2804ed2046
Johannes Stein <johannesstein@freeze-dev.com>
2013-08-26 22:29:50 +0200

    Updated dist JS

--- dist/js/lyria.js
+++ dist/js/lyria.js
@@ -797,6 +797,30 @@ define('fisheryates', ['random'], function(random) {
   };
 });
 
+define('requestfullscreen', function() {
+  return function(element) {
+    if (element.requestFullScreen) {
+      element.requestFullScreen();
+    } else if (element.mozRequestFullScreen) {
+      element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullScreen) {
+      element.webkitRequestFullScreen();
+    }
+  };
+});
+
+define('cancelfullscreen', function() {
+  return function(element) {
+    if (element.cancelFullScreen) {
+      element.cancelFullScreen();
+    } else if (element.mozCancelFullScreen) {
+      element.mozCancelFullScreen();
+    } else if (element.webkitCancelFullScreen) {
+      element.webkitCancelFullScreen();
+    }
+  };
+});
+
 define('isemptyobject', function() {
   return function(obj) {
     if ( typeof obj !== 'object') {
