62d94dbf0561e3cbd90939d1131a682256daa14f
Johannes Stein <johannesstein@freeze-dev.com>
2013-09-09 19:28:25 +0200

    Updated dist JS

--- dist/js/lyria.js
+++ dist/js/lyria.js
@@ -169,6 +169,40 @@ define('lyria/achievement/manager', ['jquery', 'lyria/achievement', 'lyria/templ
   
 });
 
+define('lyria/animation', function() {
+  var Animation = (function() {
+    var Animation = function($elem, options) {
+      this.$elem = $elem;
+      
+      this.frame.width = 0 || options.width;
+      this.frame.height = 0 || options.height;
+      this.frame.current = 0;
+    };
+    
+    Animation.prototype.play = function() {
+      
+    };
+    
+    Animation.prototype.pause = function() {
+      
+    };
+    
+    Animation.prototype.resume = function() {
+      
+    };
+    
+    Animation.prototype.stop = function() {
+      
+    };
+    
+    Animation.prototype.reset = function() {
+      
+    };
+  })();
+  
+  return Animation;
+});
+
 /**
  * @module Lyria
  */
