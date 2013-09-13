d4fcabece07b61e4017311db85fe7c40fdfa903d
Johannes Stein <johannesstein@freeze-dev.com>
2013-09-13 22:43:34 +0200

    Updated dist JS

--- dist/js/lyria.js
+++ dist/js/lyria.js
@@ -597,6 +597,12 @@ define('nexttick', ['requestanimationframe', 'cancelanimationframe'], function(r
   };
 });
 
+define('options', ['jquery'], function($) {
+  return function(obj, defaultOptions) {
+    return $.extend(true, obj, defaultOptions);
+  };
+});
+
 define('path', function() {
   var delimiter = '/';
 
@@ -1831,7 +1837,7 @@ define('lyria/scene', ['jquery', 'mixer', 'nexttick', 'eventmap', 'lyria/gameobj
       };
 
       // Call scene
-      require(['lyria/achievements', 'lyria/log', 'lyria/component', 'lyria/gameobject', 'lyria/events', 'lyria/resource', 'lyria/loop'], function(Achievements, Log, Component, GameObject, Events, Resource, Loop) {
+      require(['lyria/achievements', 'lyria/log', 'lyria/component', 'lyria/gameobject', 'lyria/events', 'lyria/resource', 'lyria/loop', 'lyria/tween'], function(Achievements, Log, Component, GameObject, Events, Resource, Loop, Tween) {
         var LyriaObject = {
           Achievements: Achievements,
           Log: Log,
@@ -1839,7 +1845,8 @@ define('lyria/scene', ['jquery', 'mixer', 'nexttick', 'eventmap', 'lyria/gameobj
           GameObject: GameObject,
           Events: Events,
           Resource: Resource,
-          Loop: Loop
+          Loop: Loop,
+          Tween: Tween
         };
 
         if (sceneDeps.length > 0) {
@@ -2150,19 +2157,72 @@ define('lyria/template/string', function() {
   return templateString;
 });
 
-define('lyria/tween', ['eventmap', 'mixer', 'jqueryify'], function(EventMap, mixer, $fy) {
+define('lyria/tween', ['eventmap', 'mixer', 'options', 'jqueryify'], function(EventMap, mixer, options, $fy) {
   var Tween = (function() {
-    var Tween = function(elem) {
-      this.$elem = $fy(elem);
+    var Tween = function(opts) {
+      opts = options(opts, {
+        elem: null,
+        effects: [{
+          property: null,
+          target: null,
+          easing: Tween.defaults.easing,
+          duration: Tween.defaults.duration,
+          delay: Tween.defaults.delay
+        }]
+      });
       
+      this.$elem = $fy(opts.elem);
+      this.effects = opts.effects;
+
+      this.hwAccelerated = true;
+
       mixer(Tween.prototype, new EventMap());
+
+      var self = this;
+
+      this.on('start', function() {
+        if (self.$elem) {
+          return;
+        }
+        
+        var effects = [];
+        var properties = {};
+        
+        for (var i = 0, j = self.effects.length; i < j; i++) {
+          (function(item) {
+            if (item.property == null || item.target == null) {
+              return;
+            }
+            
+            item.duration = item.duration || Tween.defaults.duration;
+            item.easing = item.easing || Tween.defaults.easing;
+            item.delay = item.delay || Tween.defaults.delay;
+            
+            effects.push([item.property, item.duration, item.easing, item.delay].join(' '));
+            properties[item.property] = item.target;
+          })(self.effects[i]);
+        }
+        
+        properties['transition'] = effects.join(', ');
+        self.$elem.css(properties);
+        
+        $(document).one('transitionend', function(e) {
+          console.log(e.target);
+        });
+      });
     };
     
+    Tween.defaults = {
+      easing: 'linear',
+      duration: '300ms',
+      delay: '0ms'
+    };
+
     return Tween;
   })();
-  
+
   return Tween;
-});
+}); 
 /**
  * @namespace Lyria
  * Lyria namespace decleration
