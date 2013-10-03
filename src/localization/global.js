e78b694bb81dac1fc996e68cd8f631954d9f44a5
Johannes Stein <johannesstein@freeze-dev.com>
2013-10-03 22:47:45 +0200

    Deleted old localization API

--- src/localization/global.js
+++
@@ -1,5 +0,0 @@
-define('lyria/localization/global', ['lyria/localization', 'lyria/resource'], function(Localization, Resource) {
-  var instance = instance || new Localization(Resource.name('i18n.json'));
-  
-  return instance;
-});
