35d87c64a191a9ae7b5c8e5f7173460947667f17
Johannes Stein <johannesstein@freeze-dev.com>
2013-10-03 13:44:06 +0200

    Updated event functions, fixes #29

--- src/scene/index.js
+++ src/scene/index.js
@@ -77,6 +77,9 @@ define('lyria/scene', ['jquery', 'mixer', 'nexttick', 'eventmap', 'lyria/gameobj
 
       // Default values
       this.localization = {};
+      
+      // Default event value
+      this.defaultEvent = 'click';
 
       var langValue = Language.language;
 
@@ -111,12 +114,11 @@ define('lyria/scene', ['jquery', 'mixer', 'nexttick', 'eventmap', 'lyria/gameobj
       self.on('added', function() {
         self.refresh();
 
-        // TODO: This overwrites eventmap events. Find a better way!
-        if (self.events) {
+        if (self.DOMEvents) {
           if (options && options.isPrefab) {
-            self.events.delegate = (options.target) ? options.target : 'body';
+            self.DOMEvents.delegate = (options.target) ? options.target : 'body';
           } else {
-            self.events.delegate = '#' + sceneName;
+            self.DOMEvents.delegate = '#' + sceneName;
           }
         }
 
@@ -131,10 +133,10 @@ define('lyria/scene', ['jquery', 'mixer', 'nexttick', 'eventmap', 'lyria/gameobj
         });
 
         // Bind events
-        if (self.events && !$.isEmptyObject(self.events)) {
-          $.each(self.events, function(key, value) {
+        if (self.DOMEvents && !$.isEmptyObject(self.DOMEvents)) {
+          $.each(self.DOMEvents, function(key, value) {
             if (( typeof value === 'object') && (key !== 'delegate')) {
-              $(self.events.delegate).on(value, key, {
+              $(self.DOMEvents.delegate).on(value, key, {
                 scene: self
               });
             }
@@ -142,6 +144,7 @@ define('lyria/scene', ['jquery', 'mixer', 'nexttick', 'eventmap', 'lyria/gameobj
         }
 
         // Data binding
+        // TODO: Find a better way than using Object.watch
         if (self.template.data && !$.isEmptyObject(self.template.data)) {
           $('#' + self.name + ' [data-bind]').each(function() {
             var $dataElem = $(this);
@@ -335,16 +338,66 @@ define('lyria/scene', ['jquery', 'mixer', 'nexttick', 'eventmap', 'lyria/gameobj
       if (selector == null) {
         return;
       }
+      
+      if (typeof selector === 'object') {
+        return this.DOMEvents = selector;
+      }
 
       if (eventName == null) {
-        return this.events[selector];
+        return this.DOMEvents[selector];
       } else {
         if ( typeof eventFunction === 'function') {
-          this.events[selector] = {};
-          this.events[selector][eventName] = eventFunction;
+          this.DOMEvents[selector] = {};
+          this.DOMEvents[selector][eventName] = eventFunction;
         } else {
-          return this.events[selector][eventName];
+          return this.DOMEvents[selector][eventName];
         }
+      }
+    };
+    
+    
+    /**
+     * Sets an event to the event object (DOM events)
+     * TODO: Not completely happy with the function name
+     *
+     * @method bindEvent
+     * @param {String} selector
+     * @param {String} eventName
+     * @param {Function} eventFunction
+     */
+    Scene.prototype.bindEvent = function(selector, eventName, eventFunction) {
+      if (selector == null) {
+        return;
+      }
+      
+      if (typeof selector === 'object') {
+        this.DOMEvents = selector;
+      }
+      
+      if (typeof eventName === 'function') {
+        this.DOMEvents[selector][this.defaultEvent] = eventFunction;
+      } else {
+        this.DOMEvents[selector][eventName] = eventFunction;
+      }
+    };
+    
+    /*
+     * Unbinds a previously bound event
+     * 
+     * @method unbindEvent
+     * @param {String} selector
+     * @param {String} eventName
+     * @param {Function} eventFunction
+     */
+    Scene.prototype.unbindEvent = function(selector, eventName) {
+      if (selector == null) {
+        return;
+      }
+      
+      if (eventName == null) {
+        delete this.DOMEvents[selector];
+      } else {
+        delete this.DOMEvents[selector][eventName];
       }
     };
 
