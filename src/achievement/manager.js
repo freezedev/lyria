8eecfb8d8a1346d1d7ae9d9b6ec6da35d8236118
Johannes Stein <johannesstein@freeze-dev.com>
2013-10-06 11:07:30 +0200

    Updated template call + Simple animation

--- src/achievement/manager.js
+++ src/achievement/manager.js
@@ -1,16 +1,17 @@
 define('lyria/achievement/manager', ['jquery', 'lyria/achievement', 'lyria/template/engine', 'lyria/template/list', 'lyria/localization'], function($, Achievement, TemplateEngine, templateList, Localization) {
-  
+
   var achievementStore = {};
-  
+
   var AchievementManager = {
     localization: new Localization(),
     add: function(achievement) {
-      if (achievement instanceof Achievement) {
+      if ( achievement instanceof Achievement) {
         if (!Object.hasOwnProperty.call(achievementStore, achievement.name)) {
-          achievementStore[achievement.name] = achievement;          
+          achievementStore[achievement.name] = achievement;
         }
       }
     },
+    progressSeparator: '/',
     viewport: null,
     remove: function(achName) {
       if (Object.hasOwnProperty.call(achievementStore, achName)) {
@@ -22,48 +23,57 @@ define('lyria/achievement/manager', ['jquery', 'lyria/achievement', 'lyria/templ
     },
     show: function(achName) {
       var currentAchievement = achievementStore[achName];
-      
+
       if (currentAchievement == null) {
-        throw new Error('Achievement ' + achName + ' not found.');        
+        throw new Error('Achievement ' + achName + ' not found.');
       }
-      
-      var title = (AchievementManager.localization.exists(currentAchievement.name)) ? Achievement.localization.t(currentAchievement.name) : currentAchievement.name;
-      var description = (AchievementManager.localization.exists(currentAchievement.name + '-description')) ? Achievement.localization.t(currentAchievement.name + '-description') : currentAchievement.description;
-      
+
+      var title = (AchievementManager.localization.exists(currentAchievement.name)) ? AchievementManager.localization.t(currentAchievement.name) : currentAchievement.name;
+      var description = (AchievementManager.localization.exists(currentAchievement.name + '-description')) ? AchievementManager.localization.t(currentAchievement.name + '-description') : currentAchievement.description;
+
       var achTemplate = TemplateEngine.compile(templateList['achievement'])({
         id: currentAchievement.id,
+        className: currentAchievement.name,
         title: title,
         description: description,
-        offscreen: true
+        offscreen: true,
+        progressable: (currentAchievement.progress.max > 0 && currentAchievement.progress.max !== 1),
+        max: currentAchievement.progress.max,
+        current: currentAchievement.progress.current,
+        separator: AchievementManager.progressSeparator
       });
-      
+
       var $currentAchievement = $('#' + currentAchievement.id);
-      
+
       if (AchievementManager.viewport == null) {
-        $('body').append(achTemplate);        
+        $('body').append(achTemplate);
       } else {
         AchievementManager.viewport.$element.append(achTemplate);
       }
-      
-      $currentAchievement.removeClass('offscreen').delay(2000).addClass('offscreen');
-      $currentAchievement.on('transitionend', function() {
+
+      // Refactor this if lyria/tween is available
+      $currentAchievement.animate({
+        opacity: 1.0
+      }, 600).delay(2000).animate({
+        opacity: 0.0
+      }, 600, function() {
         $currentAchievement.remove();
       });
     },
     toJSON: function() {
       var key, value;
       var result = {};
-      
+
       for (key in achievementStore) {
         value = achievementStore[key];
         result[key] = value.toJSON();
       }
-      
+
       return result;
     },
     toString: function() {
       var result = '';
-      
+
       try {
         result = JSON.stringify(AchievementManager.toJSON());
       } catch (e) {
@@ -73,7 +83,7 @@ define('lyria/achievement/manager', ['jquery', 'lyria/achievement', 'lyria/templ
     },
     fromJSON: function(achievements) {
       var key, value;
-      
+
       for (key in achievements) {
         value = achievements[key];
         AchievementManager.add(value);
@@ -81,38 +91,37 @@ define('lyria/achievement/manager', ['jquery', 'lyria/achievement', 'lyria/templ
     },
     fromString: function(achievements) {
       var deserializedValue = {};
-      
+
       try {
         deserializedValue = JSON.parse(achievements);
       } catch (e) {
         throw new Error('Error while deserializing achivements in AchievementManager: ' + e);
       }
-      
+
       return AchievementManager.fromJSON(deserializedValue);
     }
   };
-  
+
   Object.defineProperty(AchievementManager, 'length', {
     get: function() {
       return Object.keys(achievementStore).length;
     }
   });
-  
+
   Object.defineProperty(AchievementManager, 'unlockedCount', {
     get: function() {
       var counter = 0;
-      
+
       $.each(achievementStore, function(key, value) {
         if (value.unlocked) {
           counter++;
         }
       });
-      
+
       return counter;
     }
   });
-  
-  
+
   return AchievementManager;
-  
+
 });
