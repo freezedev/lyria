5b4a8c7ed11f993b777801cb237aae81533b4c6c
Johannes Stein <johannesstein@freeze-dev.com>
2013-10-07 22:54:26 +0200

    First shot at prefabs

--- src/prefab/index.js
+++ src/prefab/index.js
@@ -6,20 +6,6 @@ define('lyria/prefab', ['lyria/scene'], function(Scene) {
 	'use strict';
 
 	//Lyria.Prefab
-	return function(prefabName, options) {
-		
-		var defaultOptions = {
-			target: null,
-			template: 'prefab.html',
-			data: 'prefab.js',
-			path: 'prefab',
-			isPrefab: true
-		};
-		
-		options = _.defaults(true, defaultOptions, options);
-		
-		return new Scene(prefabName, options);
-		
-	};
+	return Scene;
   
 });
