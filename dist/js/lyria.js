f155a38fb80e9445fcd8a4955d39d4f83cf2749f
Johannes Stein <johannesstein@freeze-dev.com>
2013-10-07 22:55:20 +0200

    Updated dist JS

--- dist/js/lyria.js
+++ dist/js/lyria.js
@@ -1309,32 +1309,51 @@ define('lyria/prefab', ['lyria/scene'], function(Scene) {
 	'use strict';
 
 	//Lyria.Prefab
-	return function(prefabName, options) {
-		
-		var defaultOptions = {
-			target: null,
-			template: 'prefab.html',
-			data: 'prefab.js',
-			path: 'prefab',
-			isPrefab: true
-		};
-		
-		options = _.defaults(true, defaultOptions, options);
-		
-		return new Scene(prefabName, options);
-		
-	};
+	return Scene;
   
 });
-define('lyria/prefab/manager', function() {
+define('lyria/prefab/manager', ['jqueryify', 'jquery', 'root'], function($ify, $, root) {
   var PrefabManager = {};
-  
-  PrefabManager.create = function(name) {
-    if (PrefabManager.precompiledPrefabs) {
-      return PrefabManager.precompiledPrefabs[name];
-    }
+
+  PrefabManager.prefabs = {};
+  PrefabManager.viewport = null;
+  PrefabManager.className = 'prefab';
+
+  var createElement = function(type) {
+    return function(name, parent) {
+      if (parent == null) {
+        parent = ((PrefabManager.viewport) ? PrefabManager.viewport.$element :
+        void 0) || 'body';
+      }
+      
+      var prefab = null;
+      
+      if (!PrefabManager.prefabs[name]) {
+        throw new Error('No valid prefab called ' + name + ' found.');
+      }
+
+      var $parent = $ify(parent);
+
+      if ($parent) {
+        if ($('#' + prefab.name).length === 0) {
+          $parent[type]($(root.document.createElement('div')).attr('id', prefab.name).attr('class', PrefabManager.className));
+        }
+
+        if (!prefab.isAsync) {
+          prefab.trigger('added');
+        }
+      }
+    };
   };
-  
+
+  PrefabManager.append = function() {
+    createElement('append').apply(this, arguments);
+  };
+
+  PrefabManager.prepend = function() {
+    createElement('prepend').apply(this, arguments);
+  };
+
   return PrefabManager;
 });
 
