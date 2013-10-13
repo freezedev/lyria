56d5873fa945bb2f3dc9f0ebaeaa4615d398e4fc
Johannes Stein <johannesstein@freeze-dev.com>
2013-10-13 19:43:49 +0200

    Using jsbeautify to beautify handlebars output

--- tasks/handlebars.js
+++ tasks/handlebars.js
@@ -1,24 +1,26 @@
-var handlebars = require('handlebars');
-var packageFile = require('../package.json');
 var path = require('path');
 
+var handlebars = require('handlebars');
+var beautify = require('js-beautify').js_beautify;
+
+var packageFile = require('../package.json');
+
 module.exports = function(grunt) {
-  
-  
+
   grunt.registerMultiTask('handlebars', function() {
     var options = this.options({
       namespace: packageFile.name
     });
     var files = this.files;
     var dest = null;
-    
+
     var lines = '';
-    
+
     lines += 'define(\'' + options.namespace + '\', {';
     for (var i = 0, j = files.length; i < j; i++) {
       (function(item) {
         dest = dest || item.dest;
-        
+
         for (var k = 0, l = item.src.length; k < l; k++) {
           (function(filename, lastElem) {
             var shortName = path.basename(filename).split(path.extname(filename))[0];
@@ -29,10 +31,14 @@ module.exports = function(grunt) {
     }
     // Remove last comma
     lines = lines.substring(0, lines.length - 1);
-    
+
     lines += '});';
-    
+
+    lines = beautify(lines, {
+      indent_size: 2
+    });
+
     grunt.file.write(dest, lines);
   });
-  
+
 };
