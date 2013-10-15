180284a5aabdb418e127ebb9789e3ffaae0cc4d9
Johannes Stein <johannesstein@freeze-dev.com>
2013-10-15 21:56:23 +0200

    Updated eventmap mixin in lyria/world

--- src/world.js
+++ src/world.js
@@ -3,7 +3,7 @@ define('lyria/world', ['mixer', 'eventmap'], function(mixer, EventMap) {
   return (function() {
 
     var World = function() {
-      mixer(World.prototype, new EventMap());
+      mixer([this, World.prototype], new EventMap());
     };
 
     return World;
