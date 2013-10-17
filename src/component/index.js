54ff7154c432e38df9a75c5bd602d0c7660bb73d
Johannes Stein <johannesstein@freeze-dev.com>
2013-10-17 20:30:29 +0200

    First steps of component concept

---
+++ src/component/index.js
@@ -0,0 +1,81 @@
+define('lyria/component', ['mixer', 'eventmap', 'lyria/component/manager', 'lyria/log'], function(mixer, EventMap, ComponentManager, Log) {
+
+  //Lyria.Component
+  return (function() {
+
+    function Component(name, factory) {
+      mixer([this, Component.prototype], new EventMap());
+      
+      this.name = name != null ? name : this.constructor.name;
+      
+      this.type = 'Component';
+      
+      this.children = {};
+      
+      factory.apply(this, [this]);
+      
+      this.on('update', function(dt) {
+        for (var key in children) {
+          var value = children[key];
+          if (value && value.trigger) {
+            value.trigger('update', dt);
+          }
+        }
+      });
+    }
+    
+    /**
+     * Adds a component to this entity
+     * 
+     * @method add
+     * @param {Object} child
+     */
+    Component.prototype.add = function(child) {
+      var name = 'anonymous-' + Date.now();
+      var childObject = null;
+      
+      if (typeof child === 'string') {
+        name = child;
+        childObject = ComponentManager.get(name);
+      } else {
+        name = child.name || name;
+        childObject = child;
+      }
+      
+      children[name] = childObject;
+      
+      this.trigger('add', name);
+      childObject.trigger('added', this);
+    };
+    
+    /**
+     * Removes a component from the entity
+     *  
+     * @method remove
+     * @param {String} name
+     */
+    Component.prototype.remove = function(name) {
+      if (name == null) {
+        return;
+      }
+      
+      delete children[name];
+      
+      this.trigger('remove', name);
+    };
+    
+    /**
+     * Logs from the component itself
+     * 
+     * @method log
+     * @param {String} text
+     */
+    Component.prototype.log = function(text) {
+      Log.i(this.type + ': ' + text);
+    };
+
+    return Component;
+
+  })();
+
+});
---
+++ src/component/manager.js
@@ -0,0 +1,27 @@
+define('lyria/component/manager', function() {
+
+  var components = {};
+
+  var ComponentManager = {};
+
+  ComponentManager.add = function(component) {
+    var name = component.name || 'anonymous-' + Date.now();
+
+    components[name] = component;
+  };
+
+  ComponentManager.remove = function(name) {
+    if (name == null) {
+      return;
+    }
+    
+    delete ComponentManager[name];
+  };
+  
+  ComponentManager.get = function(name) {
+    return components[name];
+  }
+
+  return ComponentManager;
+
+});
