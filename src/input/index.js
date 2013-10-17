e8974744a6eafb5b1402bd8c84aa5a03c398c626
Johannes Stein <johannesstein@freeze-dev.com>
2013-10-17 20:45:28 +0200

    InputManager stub

--- src/input/index.js
+++ src/input/index.js
@@ -1,5 +1,24 @@
 define('lyria/input', function() {
-  var Input = {
-
+  var Input = {};
+  
+  Input.key = {};
+  Input.mouse = {};
+  
+  Input.key.down = function(key) {
+    
   };
+  
+  Input.key.up = function(key) {
+    
+  };
+  
+  Input.mouse.down = function() {
+    
+  };
+  
+  Input.mouse.up = function() {
+    
+  };
+  
+  return Input;
 });
