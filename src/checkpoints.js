a2ddb851c38566c77dcf50cbdef4cdb30da81f90
Johannes Stein <johannesstein@freeze-dev.com>
2013-10-19 20:30:18 +0200

    Added checkpoint system (Draft)

---
+++ src/checkpoints.js
@@ -0,0 +1,34 @@
+define('lyria/checkpoints', ['eventmap', 'mixer', 'deleteitem'], function(EventMap, mixer, deleteItem) {
+
+  var Checkpoints = (function() {
+    var Checkponts = function() {
+      mixer([this, Checkpoints.prototype], new EventMap());
+
+      this.startTime = performance.now();
+      this.checkpointList = [];
+    };
+
+    Checkpoints.prototype.pass = function(name) {
+      this.checkpointList.push(name);
+      this.trigger('pass', name, performance.now() - startTime);
+    };
+
+    Checkpoints.prototype.hasPassed = function(name) {
+      return (this.checkpointList.indexOf(name) !== -1);
+    };
+
+    Checkpoints.prototype.reset = function(name) {
+      var index = this.checkpointList.indexOf(name);
+
+      if (index === -1) {
+        return;
+      }
+
+      this.checkpointList = deleteItem(this.checkpointList, index);
+      this.trigger('reset', name);
+    };
+
+    return Checkpoints;
+  });
+
+});
