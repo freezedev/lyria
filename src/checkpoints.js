9924235b02903f6ff019637cf47157648ea49160
Johannes Stein <johannesstein@freeze-dev.com>
2013-10-21 20:39:46 +0200

    Updated inline doc + Allows Checkpoints#reset to reset everything

--- src/checkpoints.js
+++ src/checkpoints.js
@@ -8,16 +8,37 @@ define('lyria/checkpoints', ['eventmap', 'mixer', 'deleteitem'], function(EventM
       this.checkpointList = [];
     };
 
+    /**
+     * Pass a checkpoint
+     * 
+     * @param {String} name
+     */
     Checkpoints.prototype.pass = function(name) {
       this.checkpointList.push(name);
       this.trigger('pass', name, performance.now() - startTime);
     };
 
+    /**
+     * Passes a checkpoint
+     * 
+     * @param {String} name
+     */
     Checkpoints.prototype.hasPassed = function(name) {
       return (this.checkpointList.indexOf(name) !== -1);
     };
 
+    /**
+     * Reset a single or all checkpoints
+     * 
+     * @param {String} name
+     */
     Checkpoints.prototype.reset = function(name) {
+      if (name == null) {
+        this.checkpointList = [];
+        this.trigger('reset');
+        return;
+      }
+      
       var index = this.checkpointList.indexOf(name);
 
       if (index === -1) {
