ad3d8957b06f85b8c2cbd6a01d9024299215624d
Johannes Stein <johannesstein@freeze-dev.com>
2013-10-21 20:39:10 +0200

    Some TODO for the road

--- src/prefab/index.js
+++ src/prefab/index.js
@@ -5,6 +5,8 @@
 define('lyria/prefab', ['lyria/scene'], function(Scene) {
 	'use strict';
 
+  // TODO: Allow own Prefab.requireAlways similar to Scene.requireAlways
+
 	//Lyria.Prefab
 	return Scene;
   
