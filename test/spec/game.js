5f41abb25927ac15ef8f79836a8fbc9ddabc3e59
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-10 22:29:09 +0100

    Updated unit test to actually test something

--- test/spec/game.js
+++ test/spec/game.js
@@ -1,7 +1,15 @@
 define('spec/game', ['lyria/game'], function(Game) {
   
+  var game = new Game();
+  
   describe('lyria/game', function() {
+    it('is a function', function() {
+      expect(Game).to.be.a('function');
+    });
     
+    it('can be instantiated', function() {
+      expect(game).to.be.a('object');
+    });
   });
   
 });
