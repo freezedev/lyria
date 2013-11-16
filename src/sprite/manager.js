db204412cf308beeb594bb692199ec8325739605
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-16 20:59:47 +0100

    Sprite manager stub

---
+++ src/sprite/manager.js
@@ -0,0 +1,15 @@
+define('lyria/sprite/manager', ['jquery', 'lyria/sprite/renderer'], function($, Renderer) {
+  
+  var SpriteManager = (function() {
+    
+    var SpriteManager = function() {
+      
+    };
+    
+    return SpriteManager;
+    
+  })();
+  
+  return SpriteManager;
+  
+});
