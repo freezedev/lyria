7e320bcf940f9074cdff7bc8b7b6b56c1cafd9ca
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-16 21:03:27 +0100

    Mixing in component in SpriteManager

--- src/sprite/manager.js
+++ src/sprite/manager.js
@@ -1,9 +1,12 @@
-define('lyria/sprite/manager', ['jquery', 'lyria/sprite/renderer'], function($, Renderer) {
+define('lyria/sprite/manager', ['jquery', 'mixer', 'lyria/component', 'lyria/sprite/renderer'], function($, mixer, Component, Renderer) {
   
   var SpriteManager = (function() {
     
     var SpriteManager = function() {
+      var type = 'SpriteManager';
       
+      mixer([this, SpriteManager.prototype], new Component(type));
+      this.type = type;
     };
     
     return SpriteManager;
