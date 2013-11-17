a931f57b2ab1e1035798194748602dd9cf427b93
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-17 22:20:21 +0100

    Added resource test

---
+++ test/spec/resource.js
@@ -0,0 +1,35 @@
+define('spec/resource', ['lyria/resource'], function(Resource) {
+  
+  describe('lyria/resource', function() {
+    
+    it('is an object', function() {
+      expect(Resource).to.be.a('object');
+    });
+    
+    it('.path', function() {
+      expect(Resource).to.have.property('path');
+      expect(Resource.path).to.be.a('object');
+    });
+    
+    describe('.name', function() {
+      it('does exist', function() {
+        expect(Resource).to.have.property('name');
+        expect(Resource.name).to.be.a('function');
+      });
+      
+      it('calling without parameters', function() {
+        expect(Resource.name()).to.equal(undefined);
+      });
+      
+      it('calling without type parameter', function() {
+        expect(Resource.name('test')).to.be.a('string');
+      });
+      
+      it('calling with filename and type parameter', function() {
+        expect(Resource.name('test', 'image')).to.be.a('string');
+      });
+    });
+    
+  });
+  
+});
