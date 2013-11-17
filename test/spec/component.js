0f60d5e74ebb8da4f9748e7184d28a35d92e577a
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-17 22:05:55 +0100

    Added component test

---
+++ test/spec/component.js
@@ -0,0 +1,18 @@
+define('spec/component', ['lyria/component'], function(Component) {
+  
+  describe('lyria/component', function() {
+    
+    var component = new Component();
+    
+    it('is a function', function() {
+      expect(Component).to.be.a('function');
+    });
+    
+    it('can be instantiated', function() {
+      expect(component).to.be.a('object');
+      expect(component).to.be.an.instanceOf(Component);
+    });
+    
+  });
+  
+});
