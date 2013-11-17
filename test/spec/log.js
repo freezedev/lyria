185a0e5522422f40d9cddd5900c5e011934a709d
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-17 23:07:15 +0100

    Added log test

---
+++ test/spec/log.js
@@ -0,0 +1,43 @@
+define('spec/log', ['lyria/log'], function(Log) {
+  
+  describe('lyria/log', function() {
+    
+    it('is an object', function() {
+      expect(Log).to.be.a('object');
+    });
+    
+    describe('.plugins', function() {
+      it('property exists', function() {
+        expect(Log).to.have.property('plugins');
+        expect(Log.plugins).to.be.a('function');
+      });
+      
+      it('Console plugin does exist', function() {
+        expect(Log.plugins).to.have.property('console');
+        expect(Log.plugins.console).to.be.a('object');
+      });
+    });
+    
+    describe('.connector', function() {
+      it('property exists', function() {
+        expect(Log).to.have.property('connector');        
+      });
+      
+      it('is bound to the console plugin by default', function() {
+        expect(Log.connector).to.equal(Log.plugins.console);
+      });
+    });
+    
+    it('.logLevelMap', function() {
+      expect(Log).to.have.property('logLevelMap');
+      expect(Log.logLevelMap).to.be.a('object');
+    });
+    
+    it('.logLevel', function() {
+      expect(Log).to.have.property('logLevel');
+      expect(Log.logLevel).to.be.a('string');
+    });
+    
+  });
+  
+});
