18546ad161aa763e4946b6d5e9bc6d8976811af6
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-17 18:55:39 +0100

    Updated world test

--- test/spec/world.js
+++ test/spec/world.js
@@ -2,12 +2,15 @@ define('spec/world', ['lyria/world'], function(World) {
   
   describe('lyria/world', function() {
     
+    var world = new World();
+    
     it('is a function', function() {
       expect(World).to.be.a('function');
     });
     
     it('can be instantiated', function() {
-      
+      expect(world).to.be.a('object');
+      expect(world).to.be.an.instanceOf(World);
     });
     
   });
