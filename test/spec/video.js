c721c1d470a2a7352983e6022d8918bce143aec5
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-17 21:09:15 +0100

    Added video test

---
+++ test/spec/video.js
@@ -0,0 +1,18 @@
+define('spec/video', ['lyria/video'], function(Video) {
+  
+  describe('lyria/video', function() {
+    
+    var video = new Video();
+    
+    it('is a function', function() {
+      expect(Video).to.be.a('function');
+    });
+    
+    it('can be instantiated', function() {
+      expect(video).to.be.a('object');
+      expect(video).to.be.an.instanceOf(Video);
+    });
+    
+  });
+  
+});
