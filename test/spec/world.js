a9befea254ebc5bda18519ee158183da5375fb53
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-17 18:38:09 +0100

    Added world unit test

---
+++ test/spec/world.js
@@ -0,0 +1,15 @@
+define('spec/world', ['lyria/world'], function(World) {
+  
+  describe('lyria/world', function() {
+    
+    it('is a function', function() {
+      expect(World).to.be.a('function');
+    });
+    
+    it('can be instantiated', function() {
+      
+    });
+    
+  });
+  
+});
