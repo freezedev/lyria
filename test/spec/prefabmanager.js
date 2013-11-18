574ca143fdfd89ab6805a0952d08c9815e50cd06
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-18 20:31:33 +0100

    Updated prefab manager test

--- test/spec/prefabmanager.js
+++ test/spec/prefabmanager.js
@@ -6,6 +6,36 @@ define('spec/prefabmanager', ['lyria/prefab/manager'], function(PrefabManager) {
       expect(PrefabManager).to.be.a('object');
     });
     
+    it('.prefabs', function() {
+      expect(PrefabManager).to.have.property('prefabs');
+      expect(PrefabManager.prefabs).to.be.a('object');
+    });
+
+    it('.viewport', function() {
+      expect(PrefabManager).to.have.property('viewport');
+      expect(PrefabManager.viewport).to.equal(null);
+    });
+
+    it('.className', function() {
+      expect(PrefabManager).to.have.property('className');
+      expect(PrefabManager.className).to.be.a('string');
+    });    
+    
+    it('.append', function() {
+      expect(PrefabManager).to.have.property('append');
+      expect(PrefabManager.append).to.be.a('function');
+    });
+    
+    it('.prepend', function() {
+      expect(PrefabManager).to.have.property('prepend');
+      expect(PrefabManager.prepend).to.be.a('function');
+    });
+    
+    it('.insert', function() {
+      expect(PrefabManager).to.have.property('insert');
+      expect(PrefabManager.insert).to.be.a('function');
+    });
+    
   });
   
 });
