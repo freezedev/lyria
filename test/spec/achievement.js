fc3d462630ef7351891cab8c37c6fc95cdffa2b4
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-18 15:11:44 +0100

    Updated achievement manager test

--- test/spec/achievement.js
+++ test/spec/achievement.js
@@ -2,16 +2,32 @@ define('spec/achievement', ['lyria/achievement'], function(Achievement) {
   
   describe('lyria/achievement', function() {
     
-    var achievement = new Achievement();
-    
     it('is a function', function() {
       expect(Achievement).to.be.a('function');
     });
     
-    it('can be instantiated', function() {
-      expect(achievement).to.be.a('object');
-      expect(achievement).to.be.an.instanceOf(Achievement);
+    describe('constructor', function() {
+      it('can be instantiated', function() {
+        var achievement = new Achievement('test');
+
+        expect(achievement).to.be.a('object');
+        expect(achievement).to.be.an.instanceOf(Achievement);
+      });
+      
+      it('calling without parameters throws an error', function() {
+        expect(function() {
+          new Achievement();
+        }).to.throw(Error);
+      });
+      
+      it('calling without a name throws an error', function() {
+        expect(function() {
+          new Achievement({});
+        }).to.throw(Error);
+      });
+      
     });
+    
     
   });
   
