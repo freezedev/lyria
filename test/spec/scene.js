d24718e02c861a8d4c2ed51086527876eb4556c5
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-18 14:49:07 +0100

    Added scene test

---
+++ test/spec/scene.js
@@ -0,0 +1,18 @@
+define('spec/scene', ['lyria/scene'], function(Scene) {
+  
+  describe('lyria/scene', function() {
+    
+    var scene = new Scene();
+    
+    it('is a function', function() {
+      expect(Scene).to.be.a('function');
+    });
+    
+    it('can be instantiated', function() {
+      expect(scene).to.be.a('object');
+      expect(scene).to.be.an.instanceOf(Scene);
+    });
+    
+  });
+  
+});
