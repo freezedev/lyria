ca66a599201cf58c6543318933474666ce180017
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-18 14:48:32 +0100

    Added scene director test

---
+++ test/spec/scenedirector.js
@@ -0,0 +1,18 @@
+define('spec/scenedirector', ['lyria/scene/director'], function(SceneDirector) {
+  
+  describe('lyria/scene/director', function() {
+    
+    var sceneDirector = new SceneDirector();
+    
+    it('is a function', function() {
+      expect(SceneDirector).to.be.a('function');
+    });
+    
+    it('can be instantiated', function() {
+      expect(sceneDirector).to.be.a('object');
+      expect(sceneDirector).to.be.an.instanceOf(SceneDirector);
+    });
+    
+  });
+  
+});
