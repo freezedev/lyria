6923da99c939d5a2d2862198b9f8c964442f88be
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-18 14:42:10 +0100

    Added audio test

---
+++ test/spec/audio.js
@@ -0,0 +1,11 @@
+define('spec/audio', ['lyria/audio'], function(Audio) {
+  
+  describe('lyria/audio', function() {
+    
+    it('is an object', function() {
+      expect(Audio).to.be.a('function');
+    });
+    
+  });
+  
+});
