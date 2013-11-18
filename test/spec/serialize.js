efd44fa4269609e5b5d2f4196268ff7d6d11f579
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-18 13:45:02 +0100

    Updated serialize test

--- test/spec/serialize.js
+++ test/spec/serialize.js
@@ -1,15 +1,15 @@
 define('spec/serialize', ['lyria/serialize', 'jquery'], function(serialize, $) {
-  
+
   describe('serialize', function() {
-    
+
     it('is a function', function() {
       expect(serialize).to.be.a('function');
     });
-    
+
     it('should be undefined when calling without parameters', function() {
-      expect(serialize()).to.equal(undefined);      
+      expect(serialize()).to.equal(undefined);
     });
-    
+
     it('should return the primitive input parameter as a string', function() {
       expect(serialize('test')).to.equal('"test"');
       expect(serialize(0)).to.equal('0');
@@ -17,10 +17,38 @@ define('spec/serialize', ['lyria/serialize', 'jquery'], function(serialize, $) {
       expect(serialize(false)).to.equal('false');
     });
     
+    it('returns a stringified function when input parameter is a function', function() {
+      var func = function() {
+        return 5;
+      };
+      
+      expect(serialize(func)).to.equal(func.toString());
+    });
+
     it('should be undefined when input is a jquery object', function() {
       expect(serialize($('body'))).to.equal(undefined);
     });
-    
+
+    it('jquery objects serialize to null', function() {
+      expect(serialize({
+        elem: $('body')
+      })).to.deep.equal(JSON.stringify({
+        elem: null
+      }));
+    });
+
+    it('serializes primitive types', function() {
+      expect(serialize({
+        a: 1,
+        b: 'test',
+        c: false
+      })).to.deep.equal(JSON.stringify({
+        a: 1,
+        b: 'test',
+        c: false
+      }));
+    });
+
   });
-  
+
 });
