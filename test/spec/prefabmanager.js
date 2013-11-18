f9cea886bb7b9c2b66da5a43bf056a6f9a6dee8c
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-18 20:19:56 +0100

    Added prefab manager test

---
+++ test/spec/prefabmanager.js
@@ -0,0 +1,11 @@
+define('spec/prefabmanager', ['lyria/prefab/manager'], function(PrefabManager) {
+  
+  describe('lyria/prefab/manager', function() {
+    
+    it('is an object', function() {
+      expect(PrefabManager).to.be.a('object');
+    });
+    
+  });
+  
+});
