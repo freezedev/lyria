5c6e61399cc00d3513abc1f54a263679cb89dade
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-18 15:18:20 +0100

    Added component manager test

---
+++ test/spec/componentmanager.js
@@ -0,0 +1,11 @@
+define('spec/componentmanager', ['lyria/component/manager'], function(ComponentManager) {
+  
+  describe('lyria/component/manager', function() {
+    
+    it('is an object', function() {
+      expect(ComponentManager).to.be.a('object');
+    });
+    
+  });
+  
+});
