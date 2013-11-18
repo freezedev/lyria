b3670a543cf1f58dea91f6907c28f02492b48e21
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-18 14:26:07 +0100

    Added input key test

---
+++ test/spec/inputkey.js
@@ -0,0 +1,48 @@
+define('spec/inputkey', ['lyria/input/key'], function(Key) {
+  
+  describe('lyria/input/key', function() {
+    
+    it('is an object', function() {
+      expect(Key).to.be.a('object');
+    });
+    
+    describe('.define', function() {
+      it('exists', function() {
+        expect(Key).to.have.property('define');
+        expect(Key.define).to.be.a('function');
+      });
+      
+      it('without parameter', function() {
+        var keyLength = Object.keys(Key).length;
+        
+        expect(Key.define()).to.equal(undefined);
+        expect(keyLength).to.equal(Object.keys(Key).length);
+      });
+      
+      it('without key parameter', function() {
+        var keyLength = Object.keys(Key).length;
+        
+        expect(Key.define('test')).to.equal(undefined);
+        expect(keyLength).to.equal(Object.keys(Key).length);
+      });
+      
+      it('with name and key', function() {
+        expect(Key.define('action', 20)).to.equal(undefined);
+        expect(Key).to.have.property('action');
+        expect(Key.action).to.equal(20);
+      });
+      
+      it('does not overwrite existing keys', function() {
+        expect(Key.define('a', 20)).to.equal(undefined);
+        expect(Key.a).to.not.equal(20);
+      });
+      
+      it('converts functions into a getter', function() {
+        expect(Key.define('fire', function() { return 50; })).to.equal(undefined);
+        expect(Key.fire).to.equal(50);
+      });
+    });
+    
+  });
+  
+});
