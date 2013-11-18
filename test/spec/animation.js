a3c5dd8595c3f5a9ca1040c358b9712307a2f7f1
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-18 13:08:46 +0100

    Updated animation test

--- test/spec/animation.js
+++ test/spec/animation.js
@@ -8,10 +8,25 @@ define('spec/animation', ['lyria/animation'], function(Animation) {
       expect(Animation).to.be.a('function');
     });
     
-    it('can be instantiated', function() {
-      expect(animation).to.be.a('object');
-      expect(animation).to.be.an.instanceOf(Animation);
+    describe('constructor', function() {
+      it('can be instantiated', function() {
+        expect(animation).to.be.a('object');
+        expect(animation).to.be.an.instanceOf(Animation);
+      });
+      
+      it('has default values', function() {
+        expect(animation).to.have.property('frame');
+        expect(animation.frame).to.deep.equal({
+          width: 0,
+          height: 0,
+          current: 0
+        });
+        expect(animation).to.have.property('speed');
+        expect(animation.speed).to.be.a('number');
+        expect(animation.speed).to.equal(1);
+      });
     });
+    
     
   });
   
