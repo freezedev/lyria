32981ea07302bcf71544f5ee5f9637e6b5f22682
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-18 14:04:05 +0100

    Added spritemanager test

---
+++ test/spec/spritemanager.js
@@ -0,0 +1,18 @@
+define('spec/spritemanager', ['lyria/sprite/manager'], function(SpriteManager) {
+  
+  describe('lyria/sprite/manager', function() {
+    
+    var spriteManager = new SpriteManager();
+    
+    it('is a function', function() {
+      expect(SpriteManager).to.be.a('function');
+    });
+    
+    it('can be instantiated', function() {
+      expect(spriteManager).to.be.a('object');
+      expect(spriteManager).to.be.an.instanceOf(SpriteManager);
+    });
+    
+  });
+  
+});
