d047295394fc466e31210a44dd5b1379ae1ed5a5
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-18 14:36:42 +0100

    Added localization group test

---
+++ test/spec/localizationgroup.js
@@ -0,0 +1,18 @@
+define('spec/localizationgroup', ['lyria/localization/group'], function(LocalizationGroup) {
+  
+  describe('lyria/localization', function() {
+    
+    var group = new LocalizationGroup();
+    
+    it('is a function', function() {
+      expect(LocalizationGroup).to.be.a('function');
+    });
+    
+    it('can be instantiated', function() {
+      expect(group).to.be.a('object');
+      expect(group).to.be.an.instanceOf(LocalizationGroup);
+    });
+    
+  });
+  
+});
