ff45574248990cc7cc9b03861a00688c436defd4
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-18 14:42:48 +0100

    Added audio manager test

---
+++ test/spec/audiomanager.js
@@ -0,0 +1,11 @@
+define('spec/audiomanager', ['lyria/audio/manager'], function(AudioManager) {
+  
+  describe('lyria/audio/manager', function() {
+    
+    it('is an object', function() {
+      expect(AudioManager).to.be.a('object');
+    });
+    
+  });
+  
+});
