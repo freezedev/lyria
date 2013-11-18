296f48cdab845d7197016441981f7458d1cf661e
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-18 13:32:33 +0100

    Updated serialize test

--- test/spec/serialize.js
+++ test/spec/serialize.js
@@ -11,10 +11,10 @@ define('spec/serialize', ['lyria/serialize', 'jquery'], function(serialize, $) {
     });
     
     it('should return the primitive input parameter as a string', function() {
-      expect(serialize('test')).to.equal("'test'");
-      expect(serialize(0)).to.equal("0");
-      expect(serialize(true)).to.equal("true");
-      expect(serialize(false)).to.equal("false");
+      expect(serialize('test')).to.equal('"test"');
+      expect(serialize(0)).to.equal('0');
+      expect(serialize(true)).to.equal('true');
+      expect(serialize(false)).to.equal('false');
     });
     
     it('should be undefined when input is a jquery object', function() {
