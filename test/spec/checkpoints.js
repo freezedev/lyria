5afaebd0664a3e011824e0c7eb32f358f2d827d0
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-18 15:31:26 +0100

    Added checkpoints test

---
+++ test/spec/checkpoints.js
@@ -0,0 +1,40 @@
+define('spec/checkpoints', ['lyria/checkpoints'], function(Checkpoints) {
+  
+  describe('lyria/checkpoints', function() {
+    
+    var checkpoints = new Checkpoints();
+    
+    it('is a function', function() {
+      expect(Checkpoints).to.be.a('function');
+    });
+    
+    describe('constructor', function() {
+      it('can be instantiated', function() {
+        expect(checkpoints).to.be.a('object');
+        expect(checkpoints).to.be.an.instanceOf(Checkpoints);
+      });
+      
+      it('has default values', function() {
+        expect(checkpoints.startTime).to.be.a('number');
+        expect(checkpoints.checkpointList).to.be.a('array');
+      });
+    });
+    
+    describe('Checkpoints functionality', function() {
+      beforeEach(function() {
+        checkpoints.reset();
+      });
+      
+      describe('#pass', function() {
+        it('pass a checkpoint', function() {
+          checkpoints.pass('test');
+          
+          expect(checkpoints.checkpointsList).to.be.a('array');
+          expect(checkpoints.checkpointsList.indexOf('test')).to.equal(0);
+        });
+      });
+    });
+    
+  });
+  
+});
