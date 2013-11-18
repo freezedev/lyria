452bca9ba09bc59cd3f6fcf32d92d36fdda09bf3
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-18 21:15:15 +0100

    Updated template engine test

--- test/spec/templateengine.js
+++ test/spec/templateengine.js
@@ -3,7 +3,7 @@ define('spec/templateengine', ['lyria/template/engine'], function(TemplateEngine
   describe('lyria/template/engine', function() {
     
     it('is an object', function() {
-      expect(TemplateEngine).to.be.a('object');
+      expect(TemplateEngine).to.be.a('function');
     });
     
   });
