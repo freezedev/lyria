acc13b0bc93719302d49a7e181e0860cb47c9fe2
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-18 14:32:41 +0100

    Added localization test

---
+++ test/spec/localization.js
@@ -0,0 +1,18 @@
+define('spec/localization', ['lyria/localization'], function(Localization) {
+  
+  describe('lyria/localization', function() {
+    
+    var localization = new Localization();
+    
+    it('is a function', function() {
+      expect(Localization).to.be.a('function');
+    });
+    
+    it('can be instantiated', function() {
+      expect(localization).to.be.a('object');
+      expect(localization).to.be.an.instanceOf(Localization);
+    });
+    
+  });
+  
+});
