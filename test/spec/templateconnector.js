b40de986b3b80dc6e181a6ea25f967f54770d053
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-18 20:39:40 +0100

    Added template connector test

---
+++ test/spec/templateconnector.js
@@ -0,0 +1,17 @@
+define('spec/templateconnector', ['lyria/template/connector'], function(TemplateConnector) {
+  
+  describe('lyria/template/connector', function() {
+    
+    var connector = new TemplateConnector();
+    
+    it('is a function', function() {
+      expect(TemplateConnector).to.be.a('function');
+    });
+    
+    it('can be instantiated', function() {
+      expect(connector).to.be.a('object');
+    });
+    
+  });
+  
+});
