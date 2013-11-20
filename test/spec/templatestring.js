621c13ec9bef053ed1485588a6dc844e9ff74cb8
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-20 19:33:03 +0100

    Updated unit test for lyria/template/string

--- test/spec/templatestring.js
+++ test/spec/templatestring.js
@@ -14,13 +14,11 @@ define('spec/templatestring', ['lyria/template/string'], function(templateString
     it('.key.start', function() {
       expect(templateString.key).to.have.property('start');
       expect(templateString.key.start).to.be.a('string');
-      expect(templateString.key.start).to.equal('{{');
     });
 
     it('.key.end', function() {
       expect(templateString.key).to.have.property('end');
       expect(templateString.key.end).to.be.a('string');
-      expect(templateString.key.end).to.equal('}}');
     });
 
     describe('.process', function() {
@@ -41,13 +39,12 @@ define('spec/templatestring', ['lyria/template/string'], function(templateString
         expect(processedString).to.equal('test');
       });
 
-      //TODO: Not working with arrays atm
-      /*it('calling with an array', function() {
+      it('calling with an array', function() {
         var processedString = templateString.process('My {{0}} string with {{1}}', ['first', 'stuff']);
 
         expect(processedString).to.be.a('string');
         expect(processedString).to.equal('My first string with stuff');
-      });*/
+      });
 
       it('calling with an object', function() {
         var processedString = templateString.process('My {{bueno}} string with {{things}}', {
@@ -59,12 +56,12 @@ define('spec/templatestring', ['lyria/template/string'], function(templateString
         expect(processedString).to.equal('My first string with stuff');
       });
 
-      /*it('calling with an array and duplicate identifiers', function() {
+      it('calling with an array and duplicate identifiers', function() {
         var processedString = templateString.process('My {{0}} string with {{0}} {{1}}', ['nice', 'stuff']);
 
         expect(processedString).to.be.a('string');
         expect(processedString).to.equal('My nice string with nice stuff');
-      });*/
+      });
 
       it('calling with an object and duplicate identifiers', function() {
         var processedString = templateString.process('My {{bueno}} string with {{bueno}} {{things}}', {
