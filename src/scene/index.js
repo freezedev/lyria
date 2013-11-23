7fd583f18e59f96e3f9e3a9a61d2cfc6974c928c
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-23 20:43:01 +0100

    lyria/scene is now an anonymous module

--- src/scene/index.js
+++ src/scene/index.js
@@ -1,7 +1,7 @@
 /**
  * @module Lyria
  */
-define('lyria/scene', ['jquery', 'mixer', 'nexttick', 'lyria/component', 'lyria/gameobject', 'lyria/log', 'lyria/localization'], function($, mixer, nextTick, Component, GameObject, Log, Localization) {'use strict';
+define(['jquery', 'mixer', 'nexttick', 'lyria/component', 'lyria/gameobject', 'lyria/log', 'lyria/localization'], function($, mixer, nextTick, Component, GameObject, Log, Localization) {'use strict';
 
   var createNamespace = function(obj, chain, value) {
     var chainArr = chain.split('.');
