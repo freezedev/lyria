62238c8f9b4cd0fbc1ad8239b2630b6af6ce7a4a
Dawid Pach <polooo2@hotmail.com>
2013-12-11 21:44:26 +0100

    added some unit tests for audiomanager

--- test/spec/audiomanager.js
+++ test/spec/audiomanager.js
@@ -1,19 +1,45 @@
 define('spec/audiomanager', ['lyria/audio/manager'], function(AudioManager) {
-  
+
   describe('lyria/audio/manager', function() {
-    
-    it('is an object', function() {
+
+    var audioManager = new AudioManager();
+
+    it('is a function', function() {
       expect(AudioManager).to.be.a('function');
     });
-    
+
     describe('constructor', function() {
       it('can be instantiated', function() {
-        var audioManager = new AudioManager({});
-
         expect(audioManager).to.be.a('object');
         expect(audioManager).to.be.an.instanceOf(AudioManager);
       });
     });
+
+    describe('Methods and functionality', function() {
+      describe('#add', function() {
+        it('exists', function() {
+          expect(audioManager).to.have.property('add');
+          expect(AudioManager.prototype.add).to.be.a('function');
+          expect(audioManager.add).to.equal(AudioManager.prototype.add);
+        });
+      });
+      
+      describe('#play', function() {
+        it('exists', function() {
+          expect(audioManager).to.have.property('play');
+          expect(AudioManager.prototype.play).to.be.a('function');
+          expect(audioManager.play).to.equal(AudioManager.prototype.play);
+        });
+      });
+      
+      describe('#stop', function() {
+        it('exists', function() {
+          expect(audioManager).to.have.property('stop');
+          expect(AudioManager.prototype.stop).to.be.a('function');
+          expect(audioManager.stop).to.equal(AudioManager.prototype.stop);
+        });
+      });
+    });
   });
-  
+
 });
