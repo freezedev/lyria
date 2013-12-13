aaac237b55cca3b9f2f1d99394953e543b76e6e6
Johannes Stein <johannesstein@freeze-dev.com>
2013-12-13 23:12:15 +0100

    Added asynchronous preloader test

--- test/spec/preloader.js
+++ test/spec/preloader.js
@@ -1,91 +1,134 @@
 define('spec/preloader', ['lyria/preloader'], function(Preloader) {
-  
+
   describe('lyria/preloader', function() {
-    
+
     var preloader = new Preloader();
-    
+
     it('is a function', function() {
       expect(Preloader).to.be.a('function');
     });
-    
+
     describe('constructor', function() {
       it('lyria/game is instantiable', function() {
         expect(preloader).to.be.a('object');
         expect(preloader).to.be.an.instanceOf(Preloader);
       });
-      
+
       it('Instance has default values', function() {
-        expect(preloader.assets).to.be.empty;
+        expect(preloader.assets).to.be.empty
         expect(preloader.maxAssets).to.equal(0);
         expect(preloader.assetsLoaded).to.equal(0);
-        expect(preloader.steps).to.be.empty;
-        expect(preloader.taskList).to.be.empty;
+        expect(preloader.steps).to.be.empty
+        expect(preloader.taskList).to.be.empty
       });
     });
-    
-    
+
     it('Instance has property assets', function() {
       expect(preloader).to.have.property('assets');
       expect(preloader.assets).to.be.a('object');
     });
-    
+
     it('Instance has property maxAssets', function() {
       expect(preloader).to.have.property('maxAssets');
       expect(preloader.maxAssets).to.be.a('number');
     });
-    
+
     it('Instance has property assetsLoaded', function() {
       expect(preloader).to.have.property('assetsLoaded');
       expect(preloader.assetsLoaded).to.be.a('number');
     });
-    
+
     it('Instance has property steps', function() {
       expect(preloader).to.have.property('steps');
       expect(preloader.steps).to.be.a('array');
     });
-    
+
     it('Instance has property taskList', function() {
       expect(preloader).to.have.property('taskList');
       expect(preloader.taskList).to.be.a('array');
     });
-    
+
     it('#start', function() {
       expect(Preloader.prototype.start).to.be.a('function');
       expect(preloader).to.have.property('start');
       expect(preloader.start).to.equal(Preloader.prototype.start);
     });
-    
+
     describe('#task', function() {
       it('exists', function() {
         expect(Preloader.prototype.task).to.be.a('function');
         expect(preloader).to.have.property('start');
         expect(preloader.task).to.equal(Preloader.prototype.task);
       });
-      
+
       it('running synchronous tasks', function() {
         var preloader = new Preloader();
         var steps = 0;
-        
-        preloader.task(function() { steps++; });
-        preloader.task(function() { steps++; });
-        preloader.task(function() { steps++; });
-        
+
+        preloader.task(function() {
+          steps++;
+        });
+        preloader.task(function() {
+          steps++;
+        });
+        preloader.task(function() {
+          steps++;
+        });
+
         preloader.start();
-        
+
         expect(steps).to.be.a('number');
         expect(steps).to.equal(3);
       });
     });
-    
+
     describe('#task.async', function() {
       it('exists', function() {
         expect(Preloader.prototype.task.async).to.be.a('function');
         expect(preloader).to.have.property('start');
         expect(preloader.task.async).to.equal(Preloader.prototype.task.async);
       });
+
+      it('running asynchronous tasks', function(testDone) {
+        var preloader = new Preloader();
+        var steps = 0;
+
+        var checkIfDone = function() {
+          expect(steps).to.be.a('number');
+          expect(steps).to.equal(3);
+          testDone();
+        };
+
+        var stepFn = function() {
+          steps++;
+        };
+
+        preloader.task.async(function(done) {
+          setTimeout(function() {
+            stepFn();
+            checkIfDone();
+            done();
+          }, 200);
+        });
+        preloader.task.async(function(done) {
+          setTimeout(function() {
+            stepFn();
+            checkIfDone();
+            done();
+          }, 300);
+        });
+        preloader.task.async(function(done) {
+          setTimeout(function() {
+            stepFn();
+            checkIfDone();
+            done();
+          }, 400);
+        });
+
+        preloader.start();
+      });
     });
-    
-    
+
   });
-  
+
 });
