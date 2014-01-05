b9543c537fff3839c10a679e70139105ff45504b
Johannes Stein <johannesstein@freeze-dev.com>
2014-01-05 15:40:00 +0100

    Updated dist JS

--- dist/js/lyria.js
+++ dist/js/lyria.js
@@ -3290,6 +3290,14 @@ define('lyria/world', ['mixedice', 'eventmap'], function(mixedice, EventMap) {
     var World = function() {
       mixedice([this, World.prototype], new EventMap());
     };
+    
+    World.prototype.serialize = function() {
+      
+    };
+    
+    World.prototype.deserialize = function() {
+      
+    };
 
     return World;
 
