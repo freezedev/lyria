aa68bb1a2fcf1720d9b1d8fbcbc2d478fb0b3e06
Johannes Stein <johannesstein@freeze-dev.com>
2014-01-08 23:20:01 +0100

    lyria/world allows for observable properties

--- src/world.js
+++ src/world.js
@@ -1,22 +1,62 @@
-define(['mixedice', 'eventmap'], function(mixedice, EventMap) {
-  'use strict';
+define(['mixedice', 'eventmap'], function(mixedice, EventMap) {'use strict';
 
-  return (function() {
+  var World = (function() {
 
     var World = function() {
       mixedice([this, World.prototype], new EventMap());
+
+      this.register({
+        name: 'state',
+        value: ''
+      });
     };
-    
+
+    World.prototype.register = function(options) {
+      var name = options.name;
+      var value = options.value;
+      var isConstant = options.constant;
+
+      var self = this;
+
+      var getter = function(val) {
+        self.trigger('value', name, value);
+        self.trigger(name + ':value', value);
+
+        return val;
+      };
+
+      var setter = function(val) {
+        if (value !== val) {
+          value = val;
+
+          self.trigger('change', name, value);
+          self.trigger(name + ':change', value);
+        }
+      };
+
+      var propDef = {
+        get: getter
+      };
+
+      if (isConstant) {
+        propDef['set'] = setter;
+      }
+
+      Object.defineProperty(this, name, propDef);
+    };
+
     World.prototype.serialize = function() {
-      
+      return JSON.stringify(this);
     };
-    
+
     World.prototype.deserialize = function() {
-      
+
     };
 
     return World;
 
   })();
+  
+  return World;
 
 });
