145a383e577a5618a004098551b46f269e025ab8
Johannes Stein <johannesstein@freeze-dev.com>
2014-01-18 22:39:59 +0100

    Data-behavior shorthand

--- src/scene/index.js
+++ src/scene/index.js
@@ -414,6 +414,18 @@ define(['jquery', 'mixedice', 'nexttick', './component', './gameobject', './log'
         }
       }
     };
+    
+    /**
+     * Binds an DOM event to the specified data-behavior selector
+     *
+     * @method behavior
+     * @param {String} beviorName
+     * @param {String} eventName
+     * @param {Function} eventFunction
+     */
+    Scene.prototype.behavior = function(behaviorName, eventName, eventFunction) {
+      this.bindEvent('[data-behavior~=' + behaviorName +  ']', eventName, eventFunction);
+    };
 
     /**
      * Gets localized value
