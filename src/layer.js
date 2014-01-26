12eca8faa41c66b025e9e20c45654c7f451f9088
Johannes Stein <johannesstein@freeze-dev.com>
2014-01-26 20:17:43 +0100

    Updated documentation for lyria/layer

--- src/layer.js
+++ src/layer.js
@@ -2,14 +2,17 @@ define(['mixedice', './gameobject'], function(mixedice, GameObject) {
   'use strict';
   /**
    * @module lyria/layer
+   * @requires mixedice
+   * @requires lyria/gameobject
    */
 
   return (function() {
 
     /**
-     * @class Layer
-     * @extends Lyria.GameObject 
-     * @constructor
+     * @class
+     * @alias module:lyria/layer
+     * 
+     * @augments module:lyria/eventmap 
      */
     var Layer = function() {
       mixedice(this.prototype, new GameObject());
