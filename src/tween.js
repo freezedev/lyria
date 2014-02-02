eec900aa1dc7dbaace8075d4815471cc0a22e44d
Johannes Stein <johannesstein@freeze-dev.com>
2014-02-02 14:25:37 +0100

    Yep, I ran the code formatter

--- src/tween.js
+++ src/tween.js
@@ -1,34 +1,36 @@
-define(['eventmap', 'mixedice', 'options', 'jqueryify'], function(EventMap, mixedice, options, $fy) {
+define(['eventmap', 'mixedice', 'options', 'jqueryify'], function (EventMap, mixedice, options, $fy) {
   'use strict';
-  
+
   /**
    * @module lyria/tween
    * @requires eventmap
    * @requires mixedice
    * @requires options
-   * @requires jqueryify 
+   * @requires jqueryify
    */
-  
-  var Tween = (function() {
-    
+
+  var Tween = (function () {
+
     /**
      * @class
      * @alias module:lyria/tween
-     * 
+     *
      * @param {Object} opts
      */
-    var Tween = function(opts) {
+    var Tween = function (opts) {
       opts = options(opts, {
         elem: null,
-        effects: [{
-          property: null,
-          target: null,
-          easing: Tween.defaults.easing,
-          duration: Tween.defaults.duration,
-          delay: Tween.defaults.delay
-        }]
+        effects: [
+          {
+            property: null,
+            target: null,
+            easing: Tween.defaults.easing,
+            duration: Tween.defaults.duration,
+            delay: Tween.defaults.delay
+          }
+        ]
       });
-      
+
       this.$elem = $fy(opts.elem);
       this.effects = opts.effects;
 
@@ -38,40 +40,40 @@ define(['eventmap', 'mixedice', 'options', 'jqueryify'], function(EventMap, mixe
 
       var self = this;
 
-      this.on('start', function() {
+      this.on('start', function () {
         if (self.$elem) {
           return;
         }
-        
+
         var effects = [];
         var properties = {};
-        
+
         for (var i = 0, j = self.effects.length; i < j; i++) {
-          (function(item) {
+          (function (item) {
             if (item.property == null || item.target == null) {
               return;
             }
-            
+
             item.duration = item.duration || Tween.defaults.duration;
             item.easing = item.easing || Tween.defaults.easing;
             item.delay = item.delay || Tween.defaults.delay;
-            
+
             effects.push([item.property, item.duration, item.easing, item.delay].join(' '));
             properties[item.property] = item.target;
           })(self.effects[i]);
         }
-        
+
         properties['transition'] = effects.join(', ');
         self.$elem.css(properties);
-        
-        $(document).one('transitionend', function(e) {
+
+        $(document).one('transitionend', function (e) {
           console.log(e.target);
         });
       });
     };
-    
+
     /**
-     * @member module:lyria/tween.defaults 
+     * @member module:lyria/tween.defaults
      */
     Tween.defaults = {
       easing: 'linear',
@@ -83,4 +85,5 @@ define(['eventmap', 'mixedice', 'options', 'jqueryify'], function(EventMap, mixe
   })();
 
   return Tween;
-}); 
+
+});
