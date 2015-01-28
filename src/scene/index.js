7f4be0f84bdfcf5105d3955e47efce26a305385d
Johannes Stein <johannesstein@freeze-dev.com>
2015-01-28 22:37:42 +0100

    Allows require additionally to modules in scenes

--- src/scene/index.js
+++ src/scene/index.js
@@ -208,6 +208,9 @@ define(['require'], function(require) {'use strict';
         }
 
         context.modules = modules;
+        context.require = function(name) {
+          return modules[name];
+        };
 
         try {
           var success = sceneFunction.apply(context, [context, modules]);
